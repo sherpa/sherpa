1b73e5daf9a2b04e8e6b184aa63549f876b71d5d
Omar Laurino <olaurino@cfa.harvard.edu>
2014-04-01 11:39:01 -0400

    improve setup.py and extern build configuration
    Cleaned up setup.py and started it from the ground up.
    Added a setup_helpers module to contain the under-the-hood
    code needed by setup.py.
    We extend the build and clean Commands from setuptools and
    numpy's distutils.
    
    This commit is not stable yet, but is being pushed upstream
    so that I can test it on different platforms.

---
+++ setup_helpers.py
@@ -0,0 +1,66 @@
+#_PYTHON_INSERT_SAO_COPYRIGHT_HERE_(2007)_
+#_PYTHON_INSERT_GPL_LICENSE_HERE_
+
+###############################################################################
+#
+# Handling of header interdependencies
+#
+###############################################################################
+
+from numpy.distutils.command.build import build
+from distutils.command.clean import clean
+from subprocess import call, check_output
+from multiprocessing import cpu_count
+import os
+
+# Include directory for Sherpa headers
+sherpa_inc = ['sherpa/include', 'sherpa/utils/src']
+
+header_deps = {
+    'myArray': (),
+    'array': (),
+    'constants': (),
+    'extension': ('array',),
+    'integration': (),
+    'model_extension': ('extension', 'integration'),
+    'models': ('constants', 'utils'),
+    'stat_extension': ('extension',),
+    'stats': ('utils',),
+    'utils': ('constants','extension'),
+    'astro/models': ('constants', 'utils'),
+    'astro/utils': (),
+    'astro/xspec_extension': ('extension',),
+    }
+
+
+def get_deps(deps):
+    deps = set(deps)
+    alldeps = set()
+
+    while deps:
+        next = deps.pop()
+        if next not in alldeps:
+            alldeps.add(next)
+            deps.update(header_deps[next])
+
+    return [sherpa_inc[0] + '/sherpa/' + d + '.hh' for d in alldeps]
+
+def process_output(line):
+    print(line)
+
+class sherpa_build(build):
+    def run(self):
+        prefix=os.getcwd()
+        os.chdir('extern')
+        call(['./configure','--disable-shared','--prefix='+prefix+'/build'])
+        call(['make', '-j'+str(cpu_count()+1), 'install'])
+        os.chdir(prefix)
+        build.run(self)
+
+class sherpa_clean(clean):
+    def run(self):
+        clean.run(self)
+        prefix = os.getcwd()
+        os.chdir('extern')
+        call(['make', 'clean'])
+        os.chdir(prefix)
