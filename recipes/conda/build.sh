cddbdd5a6c8819c1d64e4d5a9b8b363fff6fcb81
olaurino <olaurino@cfa.harvard.edu>
2014-08-19 12:36:40 -0400

    fix conda build script for osx and add ds9 deps to metadata

--- recipes/conda/build.sh
+++ recipes/conda/build.sh
@@ -1,8 +1,27 @@
 cp -r $RECIPE_DIR/../../* .
 
 sed -i.orig "s|#install_dir=build|install_dir=$PREFIX|" setup.cfg
-sed -i.orig "s|#configure=None|configure=--disable-maintainer-mode --enable-shared --enable-stuberrorlib|" setup.cfg
 
-export CFLAGS="-L$PREFIX/lib"
+case $OSTYPE in
+    darwin*)
+        export CFLAGS="-isysroot /Developer/SDKs/MacOSX10.5.sdk"
+        export LDFLAGS="-m64"
+        cd extern
+        ./configure --enable-fftw --enable-region --enable-group --enable-wcs --prefix=$PREFIX --disable-maintainer-mode --enable-stuberrorlib --disable-shared --enable-shared=libgrp
+        make
+        make install
+        touch built
+        cd ..
+        export LDFLAGS="-undefined dynamic_lookup"
+        ;;
+
+    linux*)
+        export CFLAGS="-L$PREFIX/lib"
+        ;;
+
+esac
+
+unset CFLAGS
+unset LDFLAGS
 
 python setup.py install --prefix=$PREFIX
