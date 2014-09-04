cb1bf3fdd78537394547aa58ed97b3b79c3e025f
Omar Laurino <olaurino@cfa.harvard.edu>
2014-09-04 17:41:27 -0400

    add stklib code

---
+++ extern/stklib/src/logical.h
@@ -0,0 +1,14 @@
+/*_C_INSERT_SAO_COPYRIGHT_HERE_(1997-2007)_*/
+/*_C_INSERT_GPL_LICENSE_HERE_*/
+
+/*
+ * JCM logical type
+ */
+
+#ifndef LOGICAL_H
+#define LOGICAL_H
+typedef int logical;
+#define TRUE  1
+#define FALSE 0
+#endif
+
