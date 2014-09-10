dd53518e4905a02de5162ea370b5fb2d38e5a68f
Omar Laurino <olaurino@cfa.harvard.edu>
2014-09-10 11:17:16 -0400

    fix number of models in test_xspec

--- sherpa/astro/xspec/tests/test_xspec.py
+++ sherpa/astro/xspec/tests/test_xspec.py
@@ -32,7 +32,7 @@ class test_xspec(SherpaTestCase):
                 m = cls()
                 count += 1
 
-        self.assertEqual(count, 134)
+        self.assertEqual(count, 133)
 
     def test_evaluate_model(self):
         m = xs.XSbbody()
