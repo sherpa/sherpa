cb974ad1cabc5a2b197be2205b445dcdde24f26d
Omar Laurino <olaurino@cfa.harvard.edu>
2015-03-11 20:39:21 -0400

    add more copyright headers to file that were missing one

--- sherpa/optmethods/src/minpack/covar.f
+++ sherpa/optmethods/src/minpack/covar.f
@@ -1,3 +1,24 @@
+C
+C  Copyright (C) 2010  Smithsonian Astrophysical Observatory
+C
+C
+C This program is free software; you can redistribute it and/or modify
+C it under the terms of the GNU General Public License as published by
+C the Free Software Foundation; either version 3 of the License, or
+C (at your option) any later version.
+C
+C This program is distributed in the hope that it will be useful,
+C but WITHOUT ANY WARRANTY; without even the implied warranty of
+C MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+C GNU General Public License for more details.
+C
+C You should have received a copy of the GNU General Public License along
+C with this program; if not, write to the Free Software Foundation, Inc.,
+C 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+C
+
+
+
       subroutine covar(n,r,ldr,ipvt,tol,wa)
       integer n,ldr
       integer ipvt(n)
