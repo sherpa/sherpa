ec20a2571004146f1d19572aa9e8429f0b40059e
Omar Laurino <olaurino@cfa.harvard.edu>
2015-05-19 17:51:28 -0400

    update copyright years

--- sherpa/astro/ui/tests/test_nan.py
+++ sherpa/astro/ui/tests/test_nan.py
@@ -1,5 +1,5 @@
 # 
-#  Copyright (C) 2013  Smithsonian Astrophysical Observatory
+#  Copyright (C) 2013, 2015  Smithsonian Astrophysical Observatory
 #
 #
 #  This program is free software; you can redistribute it and/or modify
