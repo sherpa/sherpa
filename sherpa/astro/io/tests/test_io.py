4bba4aa470641bec0b50bd2dea9c63a03ecfcb27
Omar Laurino <olaurino@cfa.harvard.edu>
2016-01-07 15:20:02 -0500

    add tests for deprecation warnings

--- sherpa/astro/io/tests/test_io.py
+++ sherpa/astro/io/tests/test_io.py
@@ -18,17 +18,42 @@
 #
 
 from sherpa.utils import SherpaTestCase, has_fits_support, has_package_from_list
-from unittest import skipIf
-
 from sherpa.astro import ui
+
+from unittest import skipIf
+from tempfile import NamedTemporaryFile
+import warnings
 
 
 class test_89_issues(SherpaTestCase):
+    def setUp(self):
+        ui.clean()
+
     @skipIf(not has_package_from_list("sherpa.astro.xspec"), "xspec required")
     @skipIf(not has_fits_support(), "fits support required")
     def test_mod_fits(self):
-        ui.clean()
         tablemodelfile = self.make_path("xspec", "tablemodel", "RCS.mod")
         ui.load_table_model("tmod", tablemodelfile)
         tmod = ui.get_model_component("tmod")
         self.assertEqual("xstablemodel.tmod", tmod.name)
+
+    @skipIf(not has_package_from_list("sherpa.astro.io.pyfits_backend"), "this is a pyfits/astropy test")
+    def test_warnings_are_gone_arrays(self):
+        with warnings.catch_warnings(record=True) as w:
+            warnings.simplefilter("always")
+            with NamedTemporaryFile() as f:
+                ui.load_arrays(1, [1,2,3], [4,5,6])
+                ui.save_data(1, f.name, ascii=True, clobber=True)
+            with NamedTemporaryFile() as f:
+                ui.save_data(1, f.name, ascii=False, clobber=True)
+            assert len(w) == 0
+
+    @skipIf(not has_package_from_list("sherpa.astro.io.pyfits_backend"), "this is a pyfits/astropy test")
+    def test_warnings_are_gone_pha(self):
+        pha = self.make_path("threads", "pha_intro", "3c273.pi")
+        with warnings.catch_warnings(record=True) as w:
+            warnings.simplefilter("always")
+            with NamedTemporaryFile() as f:
+                ui.load_pha(pha)
+                ui.save_data(1, f.name, ascii=False, clobber=True)
+            assert len(w) == 0
