34286d1a9d7839d2e1acd1dc5ac3ea0efb01ab84
Omar Laurino <olaurino@cfa.harvard.edu>
2016-06-17 09:38:50 -0400

    integration c extension was used by other c extensions (at least models)
    This reverts commit 44acc934a272adc73ea6fe76b3d323820ffddeb6.

--- helpers/extensions/__init__.py
+++ helpers/extensions/__init__.py
@@ -255,6 +255,7 @@ static_ext_modules = [
                    saoopt,
                    tstoptfct,
                    # statfcts,
+                   integration,
                    # astro_modelfcts,
                    # pileup,
                    # astro_utils,
