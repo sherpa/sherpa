16efaada90950a9d05e89f5f490942677b66f453
Douglas Burke <dburke.gw@gmail.com>
2016-11-01 19:32:23 -0400

    Add a brief mention of the use of _renamedpars attribute

--- sherpa/models/model.py
+++ sherpa/models/model.py
@@ -119,6 +119,11 @@ class Model(NoNewAttributesAfterInit):
 
         mdl.norm = 1.2e-3
 
+    Support is provided to allow parameter names to be changed,
+    while still providing access to the old name (as a deprecated
+    interface). This is handled by creating an array of (old name,
+    new name) pairs - where old and new names are strings - and storing
+    it in the ``_renamedpars`` attribute when initialising the model.
     """
 
     def __init__(self, name, pars=()):
@@ -164,8 +169,8 @@ class Model(NoNewAttributesAfterInit):
     def __iter__(self):
         return iter([self])
 
-    # Make parameter access case insensitive
     def __getattr__(self, name):
+        """Access to parameters is case insensitive."""
         lname = name.lower()
 
         def warn(oname, nname):
