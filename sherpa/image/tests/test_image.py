be0675ec38b74febfaa8dbcecf8ddb4539494bb3
Douglas Burke <dburke.gw@gmail.com>
2017-07-05 17:02:07 -0400

    Update copyright year

--- sherpa/image/tests/test_image.py
+++ sherpa/image/tests/test_image.py
@@ -1,5 +1,5 @@
 #
-#  Copyright (C) 2007, 2015, 2016  Smithsonian Astrophysical Observatory
+#  Copyright (C) 2007, 2015, 2016, 2017  Smithsonian Astrophysical Observatory
 #
 #
 #  This program is free software; you can redistribute it and/or modify
