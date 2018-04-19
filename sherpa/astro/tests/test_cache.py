1f0cde65c3ad1b9d3f9ddd3d2b346181d3c06e62
Dan Nguyen <dnguyen@cfa.harvard.edu>
2018-04-19 12:46:31 -0400

    Added another test

--- sherpa/astro/tests/test_cache.py
+++ sherpa/astro/tests/test_cache.py
@@ -25,10 +25,10 @@ from sherpa.utils import requires_data, requires_xspec, requires_fits
 from sherpa.utils import SherpaTestCase
 
 from sherpa.models import Polynom1D, SimulFitModel
-from sherpa.astro.models import Lorentz1D
+from sherpa.models.basic import Gauss1D
 from sherpa.data import Data1D, DataSimulFit
-from sherpa.optmethods import NelderMead
-from sherpa.stats import Cash
+from sherpa.optmethods import NelderMead, LevMar
+from sherpa.stats import Cash, LeastSq
 from sherpa.fit import Fit
 
 import logging
@@ -56,49 +56,57 @@ class test_cache(SherpaTestCase):
                                 1.9361738167035432, 0.4791446119040638,
                                 1.342017385707699, 0.49194814027685246])
         }
+
+    _fit_g2g2_bench = {
+        'numpoints': 200,
+        'dof': 194,
+        'istatval': 4.274899468449953,
+        'statval': 0.504555457862299,
+        'parnames': ('gauss1d.fwhm', 'gauss1d.pos', 'gauss1d.ampl',
+                     'gauss1d.fwhm', 'gauss1d.pos', 'gauss1d.ampl'),
+        'parvals': numpy.array([1.1421173550314392, 1.1963309074397634,
+                                0.9677194890720789, 4.1688184531922765,
+                                -1.3386595864803255, 1.0047067939881642])
+        }
+
     def setUp(self):
         self._old_logger_level = logger.getEffectiveLevel()
         logger.setLevel(logging.ERROR)
 
-        x1 = [  1.,  3.,  5.,  7.,  9., 11., 13., 15., 17., 19., 21., 23.,
-                25., 27., 29., 31., 33., 35., 37., 39., 41., 43., 45.,
-                47., 49., 51., 53., 55., 57., 59., 61., 63., 65., 67.,
-                69., 71., 73., 75., 77., 79., 81., 83., 85., 87., 89.,
-                91., 93., 95., 97., 99.,101.]
+        x = numpy.linspace(1.0, 101., num=101)[0::2]
         y1 = [ 1., 5., 2., 4., 7.,11., 9., 8.,12.,18.,12.,11.,13.,12.,13.,
                13.,20.,23.,16.,20.,24.,17.,21.,26.,22.,24.,24.,21.,28.,
                28.,26.,25.,34.,26.,34.,33.,25.,38.,31.,43.,35.,42.,50.,
                41.,43.,47.,57.,53.,60.,46.,54.]
-
-        x2 = [ 1.,  3.,  5.,  7.,  9., 11., 13., 15., 17., 19., 21., 23.,
-               25., 27., 29., 31., 33., 35., 37., 39., 41., 43., 45., 47.,
-               49., 51., 53., 55., 57., 59., 61., 63., 65., 67., 69., 71.,
-               73., 75., 77., 79., 81., 83., 85., 87., 89., 91., 93., 95.,
-               97., 99.,101.]
         y2 = [ 0., 7., 6., 3., 5., 5., 9.,11.,13., 8.,14.,13.,14.,18.,11.,
                15.,17.,26., 15.,19.,25.,30.,15.,29.,16.,25.,27.,29.,36.,
                41.,22.,27.,33.,32.,45.,37.,38.,38.,34.,52.,40.,41.,31.,
                47.,38.,52.,57.,33.,48.,53.,45.]
-
-        x3 = [ 1.,  3.,  5.,  7.,  9., 11., 13., 15., 17., 19., 21., 23.,
-               25., 27., 29., 31., 33., 35., 37., 39., 41., 43., 45., 47.,
-               49., 51., 53., 55., 57., 59., 61., 63., 65., 67., 69., 71.,
-               73., 75., 77., 79., 81., 83., 85., 87., 89., 91., 93., 95.,
-               97., 99.,101.]
         y3 = [ 1., 2., 4., 2., 5., 8.,15.,10.,13.,10.,16.,10.,13.,12.,16.,
                17.,17.,20., 23.,16.,25.,22.,19.,31.,26.,24.,21.,29.,36.,
                30.,33.,30.,37.,27.,36.,32., 42.,44.,39.,30.,40.,33.,39.,
                49.,56.,47.,46.,35.,63.,40.,57.]
+        self.d1 = Data1D('1', x, y1)
+        self.d2 = Data1D('2', x, y2)
+        self.d3 = Data1D('3', x, y3)
 
-        self.d1 = Data1D('1', x1, y1)
-        self.d2 = Data1D('2', x2, y2)
-        self.d3 = Data1D('3', x3, y3)
+        x = numpy.linspace(-5., 5., 100)
+        g1, g2 = Gauss1D(), Gauss1D()
+        g1.fwhm = 1.14
+        g1.pos = 1.2
+        g2.fwhm = 4.13
+        g2.pos = -1.3
+        numpy.random.seed(0)
+        y1 = g1(x) + numpy.random.normal(0.0, 0.05, x.shape)
+        y2 = g2(x) + numpy.random.normal(0.0, 0.05, x.shape)
+        self.d4 = Data1D('4', x, y1)
+        self.d5 = Data1D('5', x, y2)
 
     def tearDown(self):
         if hasattr(self, "_old_logger_level"):
             logger.setLevel(self._old_logger_level)
 
-    def compare_results(self, arg1, arg2, tol=1e-6):
+    def compare_results(self, arg1, arg2, tol=1e-4):
 
         for key in ["numpoints", "dof"]:
             assert arg1[key] == int(getattr(arg2, key))
@@ -124,7 +132,7 @@ class test_cache(SherpaTestCase):
         poly1.pars[1].thaw()
         poly2.pars[1].thaw()
         poly3.pars[1].thaw()
-        sdata = DataSimulFit('all', (self.d1, self.d2, self.d3))
+        sdata = DataSimulFit('d123', (self.d1, self.d2, self.d3))
         smodel = SimulFitModel('diff', (poly1, poly2, poly3))
         sfit = Fit(sdata, smodel, method=NelderMead(), stat=Cash())
         result = sfit.fit()
@@ -133,8 +141,20 @@ class test_cache(SherpaTestCase):
     def test_same_cache(self):
         poly = Polynom1D()
         poly.pars[1].thaw()
-        sdata = DataSimulFit('all', (self.d1, self.d2, self.d3))
+        sdata = DataSimulFit('d1d2d3', (self.d1, self.d2, self.d3))
         smodel = SimulFitModel('same', (poly, poly, poly))
         sfit = Fit(sdata, smodel, method=NelderMead(), stat=Cash())
         result = sfit.fit()
         self.compare_results(self._fit_same_poly_bench, result)
+
+    def test_gauss_gauss(self):
+        g1, g2 = Gauss1D(), Gauss1D()
+        g1.fwhm = 1.3
+        g1.pos = 1.5
+        g2.fwhm = 4.
+        g2.pos = -2.0
+        sdata = DataSimulFit('d4d5', (self.d4, self.d5))
+        smodel = SimulFitModel('g1g2', (g1, g2))
+        sfit = Fit(sdata, smodel, method=LevMar(), stat=LeastSq())
+        result = sfit.fit()
+        self.compare_results(self._fit_g2g2_bench, result)
