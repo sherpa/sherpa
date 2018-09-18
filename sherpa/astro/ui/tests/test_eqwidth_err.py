294d82a2bc239de35c81bdfcbc6b54408c5e3c9c
Omar Laurino <olaurino@cfa.harvard.edu>
2018-09-18 16:43:52 -0400

    fix another test import

--- sherpa/astro/ui/tests/test_eqwidth_err.py
+++ sherpa/astro/ui/tests/test_eqwidth_err.py
@@ -17,7 +17,7 @@
 #  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
 
-from sherpa.utils import requires_data, requires_fits, requires_xspec
+from sherpa.utils.testing import requires_data, requires_fits, requires_xspec
 import numpy
 
 from pytest import approx
