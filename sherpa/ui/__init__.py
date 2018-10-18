75081d9d6babffaf979f4ef6f853ee4462108cc5
Doug Burke <dburke@cfa.harvard.edu>
2018-10-18 14:40:20 -0400

    Remove sherpa.utils.erf from ui.__all__

--- sherpa/ui/__init__.py
+++ sherpa/ui/__init__.py
@@ -1,5 +1,5 @@
-# 
-#  Copyright (C) 2007  Smithsonian Astrophysical Observatory
+#
+#  Copyright (C) 2007, 2018  Smithsonian Astrophysical Observatory
 #
 #
 #  This program is free software; you can redistribute it and/or modify
@@ -20,15 +20,15 @@
 import sherpa.all
 import sherpa.ui.utils
 from sherpa.utils import calc_mlr, calc_ftest, rebin, histogram1d, \
-    histogram2d, gamma, lgam, erf, igamc, igam, incbet, multinormal_pdf, \
+    histogram2d, gamma, lgam, igamc, igam, incbet, multinormal_pdf, \
     multit_pdf
 from sherpa.data import Data1D, Data1DInt, Data2D, Data2DInt
 from sherpa.logposterior import Prior
 
 # We build up __all__ as we go along
-__all__ = ['calc_mlr', 'calc_ftest','Data1D', 'Data1DInt',
+__all__ = ['calc_mlr', 'calc_ftest', 'Data1D', 'Data1DInt',
            'Data2D', 'Data2DInt', 'rebin', 'histogram1d',
-           'histogram2d','gamma', 'lgam', 'erf', 'igamc',
+           'histogram2d', 'gamma', 'lgam', 'igamc',
            'igam', 'incbet', 'Prior', 'multinormal_pdf', 'multit_pdf']
 
 _session = utils.Session()
