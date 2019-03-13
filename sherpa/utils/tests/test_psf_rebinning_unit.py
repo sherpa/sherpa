8633d40625a7c2d47d75b2eb6f038764412a7696
Omar Laurino <olaurino@cfa.harvard.edu>
2019-03-13 16:53:42 -0400

    add unit test

---
+++ sherpa/utils/tests/test_psf_rebinning_unit.py
@@ -0,0 +1,170 @@
+#
+#  Copyright (C) 2019  Smithsonian Astrophysical Observatory
+#
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License along
+#  with this program; if not, write to the Free Software Foundation, Inc.,
+#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+
+"""
+These tests exercise an issue with Sherpa summarized in
+https://github.com/sherpa/sherpa/issues/43
+
+A very simple test case which would capture this is the following.
+
+Assume you have a 2D source with a simple, circular, Gaussian emission. Assume you have a PSF defined at a
+higher resolution than the images to fit will have. And assume that this PSF is also a simple, circular
+Gaussian with different parameters than the source.
+
+We can create a simulated image IMG by convolving the model M, evaluated at the PSF's resolution,
+with the PSF, and then rebin it at the desired image resolution to obtain a new image IMG_lo. As the
+convolution is between two Gaussians, the resulting image will also be the image of a Gaussian,
+whose values we can predict, and in particular the variance of IMG will be the sum in quadrature of the
+variances of M and PSF. For simplicity, we might need to assume an exact ratio in pixel sizes.
+
+When the current Sherpa is run, it will apply the PSF to the image as if they were defined at the same
+resolution. Sherpa will overestimate the size of the PSF and thus underestimate the size of the source.
+We can analytically derive the (incorrect) fit results Sherpa would calculate now, while
+the correct results we should get after the improvements are already known by us setting up the image.
+
+The tests in this module implement the following scenario.
+
+We will keep updating the file with more tests for error handling, edge and corner cases, etc.
+
+We might have a different file for integration tests with more realistic data, as we will need
+to exercise actual images and PSFs with WCS headers.
+"""
+
+from math import sqrt
+from pytest import approx, fixture, mark
+
+import attr
+import numpy as np
+
+from sherpa.astro.ui.utils import Session
+from sherpa.astro.data import DataIMG
+from sherpa.data import Data2D
+from sherpa.astro.instrument import PSFModel
+from sherpa.models import SigmaGauss2D
+
+
+@attr.s
+class FixtureConfiguration:
+    image_size = attr.ib()
+    psf_size = attr.ib()
+    source_amplitude = attr.ib()
+    source_sigma = attr.ib()
+    psf_sigma = attr.ib()
+    resolution_ratio = attr.ib()
+
+
+def generate_fixtures():
+    return FixtureConfiguration(image_size=500, psf_size=100, source_amplitude=100,
+                                source_sigma=50, psf_sigma=5, resolution_ratio=1.5), \
+           FixtureConfiguration(image_size=300, psf_size=150, source_amplitude=100,
+                                source_sigma=1, psf_sigma=15, resolution_ratio=1.5), \
+           FixtureConfiguration(image_size=500, psf_size=100, source_amplitude=100,
+                                source_sigma=50, psf_sigma=5, resolution_ratio=2.5), \
+           FixtureConfiguration(image_size=300, psf_size=150, source_amplitude=100,
+                                source_sigma=1, psf_sigma=15, resolution_ratio=2.5), \
+           FixtureConfiguration(image_size=500, psf_size=100, source_amplitude=100,
+                                source_sigma=50, psf_sigma=5, resolution_ratio=0.5), \
+           FixtureConfiguration(image_size=300, psf_size=150, source_amplitude=100,
+                                source_sigma=1, psf_sigma=15, resolution_ratio=0.5), \
+           FixtureConfiguration(image_size=500, psf_size=100, source_amplitude=100,
+                                source_sigma=50, psf_sigma=5, resolution_ratio=0.7), \
+           FixtureConfiguration(image_size=300, psf_size=150, source_amplitude=100,
+                                source_sigma=1, psf_sigma=15, resolution_ratio=0.7),
+
+
+@mark.parametrize("psf_fixture", generate_fixtures(), indirect=True)
+def test_psf_resolution_bug(psf_fixture):
+    session, source, expected_sigma = psf_fixture
+    session.fit()
+
+    assert source.sigma_a.val == expected_sigma
+    assert source.sigma_b.val == expected_sigma
+
+
+def symmetric_gaussian_image(amplitude, sigma, position, n_bins):
+    model = SigmaGauss2D()
+    model.ampl = amplitude
+    model.sigma_a = sigma
+    model.sigma_b = sigma
+    model.xpos = position
+    model.ypos = position
+
+    arrays = np.arange(0, n_bins), np.arange(0, n_bins)
+    x_array, y_array = np.meshgrid(*arrays)
+    x_array, y_array = x_array.flatten(), y_array.flatten()
+
+    return model(x_array, y_array).flatten(), x_array, y_array
+
+
+@fixture
+def psf_fixture(request):
+    configuration = request.param
+    ui = Session()
+
+    source_position = configuration.image_size / 2
+
+    # The psf parameters in terms of the input parameters. To simulate the different pixel size
+    # we set the sigma of the psf to be a multiple (according to the ratio input)
+    # of the actual sigma in data pixel units.
+    # This should correspond, when ratio>1 to the case when the PSF's resolution is bigger
+    # than the image. If the ratio is 1 than they have the same pixel size.
+    psf_sigma = configuration.psf_sigma * configuration.resolution_ratio
+    psf_amplitude = 1
+    psf_position = configuration.psf_size / 2
+
+    # The convolution of two gaussians is a gaussian with a stddev which is the sum
+    # of those convolved, so we model the image as a Gaussian itself.
+    image_sigma = sqrt(configuration.source_sigma ** 2 + configuration.psf_sigma ** 2)
+    image_amplitude = configuration.source_amplitude * psf_amplitude
+    image, image_x, image_y = symmetric_gaussian_image(amplitude=image_amplitude, sigma=image_sigma,
+                                                       position=source_position, n_bins=configuration.image_size)
+
+    # We model the PSF as a gaussian as well. Note we are using the psf_sigma variable,
+    # that is the one which is scaled through the ration. In other terms, we are working in
+    # units of Data Pixels to simulate the conditions of the bug, when the ratio != 1.
+    psf, psf_x, psf_y = symmetric_gaussian_image(amplitude=psf_amplitude, sigma=psf_sigma,
+                                                 position=psf_position, n_bins=configuration.psf_size)
+
+    ui.load_arrays(1, image_x, image_y,
+                   image,
+                   (configuration.image_size, configuration.image_size),
+                   DataIMG)
+
+    # Create a Sherpa PSF model object using the psf arrays
+    sherpa_kernel = Data2D('kernel_data',
+                           psf_x.flatten(), psf_y.flatten(), psf.flatten(),
+                           shape=(configuration.psf_size, configuration.psf_size))
+    sherpa_psf = PSFModel('psf_model', kernel=sherpa_kernel)
+    sherpa_psf.norm = 1
+    sherpa_psf.origin = (psf_position, psf_position)
+
+    exact_expected_sigma = configuration.source_sigma
+    approx_expected_sigma = approx(exact_expected_sigma, rel=1e-4)
+
+    # Set the source model as a 2D Gaussian, and set the PSF in Sherpa
+    sherpa_source = SigmaGauss2D('source')
+    sherpa_source.ampl = configuration.source_amplitude
+    sherpa_source.sigma_a = exact_expected_sigma
+    sherpa_source.sigma_b = exact_expected_sigma
+    sherpa_source.xpos = source_position
+    sherpa_source.ypos = source_position
+    ui.set_source(sherpa_source)
+    ui.set_psf(sherpa_psf)
+
+    return ui, sherpa_source, approx_expected_sigma
