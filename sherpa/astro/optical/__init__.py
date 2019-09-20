7014700c59ffea8ce37492e3525a5269c2203276
Dan Nguyen <dnguyen@cfa.harvard.edu>
2019-09-20 15:28:56 -0400

    Remove the unused parameter in sherpa.astro.optical.LogEmission model

--- sherpa/astro/optical/__init__.py
+++ sherpa/astro/optical/__init__.py
@@ -1164,12 +1164,10 @@ class LogEmission(RegriddableModel1D):
                              units='angstroms')
         self.flux = Parameter(name, 'flux', 1.)
         self.skew = Parameter(name, 'skew', 1., tinyval, frozen=True)
-        self.limit = Parameter(name, 'limit', 4., alwaysfrozen=True,
-                               hidden=True )
 
         ArithmeticModel.__init__(self, name,
                                  (self.fwhm, self.pos, self.flux,
-                                  self.skew, self.limit))
+                                  self.skew))
 
     # @modelCacher1d
     def calc(self, p, x, xhi=None, **kwargs):
