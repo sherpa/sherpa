279e116ccf3c6403bf6cd08f0e07a9041c898314
Douglas Burke <dburke.gw@gmail.com>
2020-03-11 13:13:23 -0400

    Test: rewrite from unittest to pytest

--- sherpa/astro/utils/tests/test_astro_utils.py
+++ sherpa/astro/utils/tests/test_astro_utils.py
@@ -1,5 +1,6 @@
 #
-#  Copyright (C) 2008, 2016, 2018  Smithsonian Astrophysical Observatory
+#  Copyright (C) 2008, 2016, 2018, 2020
+#       Smithsonian Astrophysical Observatory
 #
 #
 #  This program is free software; you can redistribute it and/or modify
@@ -17,36 +18,29 @@
 #  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
 
+import pytest
 
-from sherpa.utils.testing import SherpaTestCase
 from sherpa.astro.utils import is_in
 
+LONGVALS = [100, 249, 400, 450, 500, 601, 1024]
+SHORTVALS = [100, 249, 601, 1024]
 
-class test_utils(SherpaTestCase):
 
-    def setUp(self):
-        self.long  = [100, 249, 400, 450, 500, 601, 1024]
-        self.short = [100, 249, 601, 1024]
+# test_is_in_long and test_not_in_short were originally a
+# single test case called test_response_filter_logic
+#
+@pytest.mark.parametrize("lo, hi",
+                         [(50, 2400),  # outside case
+                          (100, 200),  # lo case
+                          (50, 1024),  # hi case
+                          (250, 2000),  # 'hidden' lo case
+                          (50, 250),  # 'hidden' hi case
+                          (250, 600)  # 'hidden' interval case w/ noticed channels inside
+                         ])
+def test_is_in_long(lo, hi):
+    assert is_in(LONGVALS, lo, hi)
 
-    def test_response_filter_logic(self):
 
-        # outside case
-        self.assertTrue( is_in(self.long, 50, 2400) )
-
-        # lo case
-        self.assertTrue( is_in(self.long, 100, 200) )
-
-        # hi case
-        self.assertTrue( is_in(self.long, 50, 1024) )
-
-        # 'hidden' lo case
-        self.assertTrue( is_in(self.long, 250, 2000) )
-
-        # 'hidden' hi case
-        self.assertTrue( is_in(self.long, 50, 250) )
-
-        # 'hidden' interval case w/ noticed channels inside
-        self.assertTrue( is_in(self.long, 250, 600) )
-
-        # 'hidden' interval case w/ *no* noticed channels inside
-        self.assertTrue( not is_in(self.short, 250, 600) )
+def test_not_in_short():
+    # 'hidden' interval case w/ *no* noticed channels inside
+    assert not is_in(SHORTVALS, 250, 600)
