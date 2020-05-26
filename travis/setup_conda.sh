492884fa3150c66496acd289e293df6e86876a1f
Marie-Terrell <marie.terrell@cfa.harvard.edu>
2020-05-26 09:01:56 -0400

    Updates xspec, disables fftw for xspec jobs, removes conda-forge, and uses travis compilers for macOS

--- travis/setup_conda.sh
+++ travis/setup_conda.sh
@@ -11,41 +11,48 @@ then
     compilers="gcc_linux-64 gxx_linux-64 gfortran_linux-64"
 else  # osx
     miniconda_os=MacOSX
-    compilers="clang_osx-64 clangxx_osx-64 gfortran_osx-64"
 
-    # On macOS we also need the conda libx11 libraries used to build xspec
-    # We also need to pin down ncurses, for now only on macos.
-    xorg="xorg-libx11 ncurses=5"
+    #Conda compilers - Comment out to test non-conda compilers
+    #Unset the Travis compiler variables
+    #unset CC CFLAGS CXXFLAGS
+    #compilers="clang_osx-64 clangxx_osx-64 gfortran_osx-64"
+    #
+    ##Download and set the location of the macOS 10.9 SDK for the Conda Compilers to work
+    #mkdir -p 10.9SDK
+    #wget https://github.com/phracker/MacOSX-SDKs/releases/download/10.13/MacOSX10.9.sdk.tar.xz -O MacOSX10.9.sdk.tar.xz
+    #if [[ $? -ne 0 ]]; then
+    #  echo "macOS 10.9 SDK download failed"
+    #fi
+    #tar -C 10.9SDK -xf MacOSX10.9.sdk.tar.xz
+    #export CONDA_BUILD_SYSROOT=$(pwd)/10.9SDK/MacOSX10.9.sdk
+    #echo "CONDA_BUILD_SYROOT=${CONDA_BUILD_SYSROOT}"
+    #End of Conda compilers section
 fi
 
 # Download and install conda
-wget http://repo.continuum.io/miniconda/Miniconda3-latest-${miniconda_os}-x86_64.sh -O miniconda.sh
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-${miniconda_os}-x86_64.sh -O miniconda.sh
 chmod +x miniconda.sh
 ./miniconda.sh -b -p $miniconda
-export PATH=$miniconda/bin:$PATH
+
+#Source the Conda profile
+source $miniconda/etc/profile.d/conda.sh
 
 # update and add channels
 conda update --yes conda
 
-# Note the order of the channels matter. We built the xspec conda packages for macos using the conda-forge channel
-# with the highest priority, so we add it with a higher priority than the default channels, but with less priority
-# than our own channels, so that we don't accidentally get conda-forge packages for cfitsio/ccfits.
-#
 # To avoid issues with non-XSPEC builds (e.g.
 # https://github.com/sherpa/sherpa/pull/794#issuecomment-616570995 )
 # the XSPEC-related channels are only added if needed
 #
-if [ -n "${XSPECVER}" ]; then conda config --add channels conda-forge; fi
-
 conda config --add channels ${sherpa_channel}
 if [ -n "${XSPECVER}" ]; then conda config --add channels ${xspec_channel}; fi
-conda config --add channels anaconda
 
 # Figure out requested dependencies
 if [ -n "${MATPLOTLIBVER}" ]; then MATPLOTLIB="matplotlib=${MATPLOTLIBVER}"; fi
 if [ -n "${NUMPYVER}" ]; then NUMPY="numpy=${NUMPYVER}"; fi
+# Xspec >=12.10.1n Conda package includes wcslib & CCfits and pulls in cfitsio & fftw
 if [ -n "${XSPECVER}" ];
- then export XSPEC="xspec-modelsonly=${XSPECVER} ${xorg}";
+ then export XSPEC="xspec-modelsonly=${XSPECVER}";
 fi
 if [ "${DOCS}" == true ];
  then export DOCSBUILD="sphinx sphinx_rtd_theme graphviz";
@@ -63,7 +70,7 @@ FITSBUILD="${FITS}"
 # We create a new environment so we don't care about the python version in the root environment.
 conda create --yes -n build python=${TRAVIS_PYTHON_VERSION} pip ${MATPLOTLIB} ${NUMPY} ${XSPEC} ${FITSBUILD} ${DOCSBUILD} ${compilers}
 
-source activate build
+conda activate build
 
 # It looks like on some systems (well, linux) the F90 variable needs to be set. Not sure why
 export F90=${F77}
