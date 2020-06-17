386dbc54ef229cb3a1317e6c4bbd29feb62e00c6
Douglas Burke <dburke.gw@gmail.com>
2020-06-17 16:40:10 -0400

    Add test of multi-RMF response
    I am not 100% convinced I understand how theedge bins are calculated so
    I am treating this as a regression test (ie just flag up if we see
    any different behavior).

--- sherpa/astro/ui/tests/test_astro_ui_multi_response.py
+++ sherpa/astro/ui/tests/test_astro_ui_multi_response.py
@@ -34,6 +34,7 @@ import pytest
 
 from sherpa.astro import ui
 from sherpa.astro.instrument import create_arf
+from sherpa.astro.data import DataRMF
 
 
 def get_egrid():
@@ -88,6 +89,74 @@ def make_arf2():
     return arf1, arf2, dset
 
 
+def make_rmf(elo, ehi, offset):
+    """offset is the number of channels anove/below"""
+
+    assert elo.size == ehi.size
+    nchans = elo.size
+
+    if offset > 0:
+        nokay = nchans - offset
+
+        n_grp = np.ones(nchans, dtype=np.int16)
+        n_chan = np.ones(nchans, dtype=np.int16)
+        n_chan[offset] = 2
+        n_chan[offset + 1:] = 3
+
+        f_chan = np.ones(nchans, dtype=np.int16)
+        f_chan[offset] = 1
+        f_chan[offset + 1:] = np.arange(1, nokay, dtype=np.int16)
+
+        matrix = np.asarray([1] * offset + [0.85, 0.15] + [0.3, 0.6, 0.1] * (nokay - 1))
+
+    elif offset < 0:
+        offset = -offset
+        nokay = nchans - offset
+
+        n_grp = np.ones(nchans, dtype=np.int16)
+        n_chan = np.ones(nchans, dtype=np.int16)
+        n_chan[:nokay - 1] = 3
+        n_chan[nokay - 1] = 2
+
+        f_chan = nchans * np.ones(nchans, dtype=np.int16)
+        f_chan[:nokay - 1] = np.arange(offset, nchans - 1, dtype=np.int16)
+        f_chan[nokay - 1] = nchans - 1
+
+        matrix = np.asarray([0.3, 0.6, 0.1] * (nokay - 1) +
+                            [0.35, 0.65] +
+                            [1] * offset)
+
+    else:
+        n_grp = np.ones(nchans, dtype=np.int16)
+        n_chan = 3 * np.ones(nchans, dtype=np.int16)
+        n_chan[0] = 2
+        n_chan[-1] = 2
+        f_chan = np.arange(0, nchans, dtype=np.int16)
+        f_chan[0] = 1
+        matrix = np.asarray([0.85, 0.15] * 1 +
+                            [0.3, 0.6, 0.1] * (nchans - 2) +
+                            [0.35, 0.65] * 1)
+
+    return DataRMF("dummy", detchans=nchans,
+                   energ_lo=elo, energ_hi=ehi,
+                   n_grp=n_grp, n_chan=n_chan,
+                   f_chan=f_chan, matrix=matrix,
+                   e_min=elo, e_max=ehi)
+
+
+# The RMF has two components that are offset from each
+# other.
+#
+def make_rmf2():
+    """Create two RMFs that overlap"""
+
+    elo, ehi, dset = get_egrid()
+
+    rmf1 = make_rmf(elo, ehi, 20)
+    rmf2 = make_rmf(elo, ehi, -10)
+    return rmf1, rmf2, dset
+
+
 @pytest.mark.parametrize("id", [None, 1, "three"])
 def test_set_multi_arfs(id, clean_astro_ui):
 
@@ -113,10 +182,42 @@ def test_set_multi_arfs_reorder(id, clean_astro_ui):
     d = ui.get_data(id=id)
     assert d.response_ids == []
 
-    ui.set_arf(id=id, arf=arf1, resp_id=2)
+    ui.set_arf(id=id, arf=arf2, resp_id=2)
     assert d.response_ids == [2]
 
-    ui.set_arf(id=id, arf=arf2, resp_id=1)
+    ui.set_arf(id=id, arf=arf1, resp_id=1)
+    assert d.response_ids == [2, 1]
+
+
+@pytest.mark.parametrize("id", [None, 1, "three"])
+def test_set_multi_rmfs(id, clean_astro_ui):
+
+    rmf1, rmf2, dset = make_rmf2()
+
+    ui.set_data(id, dset)
+    d = ui.get_data(id=id)
+    assert d.response_ids == []
+
+    ui.set_rmf(id=id, rmf=rmf1, resp_id=1)
+    assert d.response_ids == [1]
+
+    ui.set_rmf(id=id, rmf=rmf2, resp_id=2)
+    assert d.response_ids == [1, 2]
+
+
+@pytest.mark.parametrize("id", [None, 1, "three"])
+def test_set_multi_rmfs_reorder(id, clean_astro_ui):
+
+    rmf1, rmf2, dset = make_rmf2()
+
+    ui.set_data(id, dset)
+    d = ui.get_data(id=id)
+    assert d.response_ids == []
+
+    ui.set_rmf(id=id, rmf=rmf2, resp_id=2)
+    assert d.response_ids == [2]
+
+    ui.set_rmf(id=id, rmf=rmf1, resp_id=1)
     assert d.response_ids == [2, 1]
 
 
@@ -189,3 +290,82 @@ def test_eval_multi_arf_reorder(clean_astro_ui):
     ui.set_arf(id=1, arf=arf1, resp_id=1)
 
     check_eval_multi_arf()
+
+
+def check_eval_multi_rmf():
+    """Test that the data is handled correctly
+
+    For use by test_eval_multi_rmf.
+    """
+
+    mdl = ui.create_model_component('const1d', 'mdl')
+    mdl.c0 = 4
+    ui.set_source(mdl)
+
+    # The analysis setting appears to depend on how the
+    # data is set up. This is just to check it is energy.
+    #
+    d = ui.get_data()
+    assert d.units == 'energy'
+
+    # Easiest way to evaluate the model is to grab the
+    # data from plot_source / plot_model
+    #
+    # The source doesn't care about how the instrument is set
+    # up.
+    #
+    splot = ui.get_source_plot()
+    assert (splot.y == 4).all()
+
+    # The model plot does care about the instrument. There should
+    # be two equal responses, offset from each other. As this is
+    # done for each energy they should "cancel out" (apart from
+    # doubling the signal)  apart from the start/end bins
+    #
+    # I haven't been able to convince myself I understand the handling
+    # at the start/end of the RMF, so I am using this just as a
+    # regression test.
+    #
+    mplot = ui.get_model_plot()
+
+    range_is_8 = np.arange(11, 968)
+    assert mplot.y[range_is_8] == pytest.approx(8)
+
+    # handle "overflow" bins
+    assert mplot.y[0] == pytest.approx(84.6)
+    assert mplot.y[-1] == pytest.approx(43.0)
+
+    # handle bins below and above the offsets
+    range_is_low = np.arange(1, 11)
+    is_low = [4.2, 4., 4., 4., 4., 4., 4., 4., 5.2, 7.6]
+    assert mplot.y[range_is_low] == pytest.approx(is_low)
+
+    range_is_high = np.arange(968, 988)
+    is_high = [6.8, 4.4, 4., 4., 4., 4., 4., 4., 4., 4.,
+               4., 4., 4., 4., 4., 4., 4., 4., 4., 4.2]
+    assert mplot.y[range_is_high] == pytest.approx(is_high)
+
+
+def test_eval_multi_rmf(clean_astro_ui):
+    """See also test_eval_multi_rmf_reorder"""
+
+    rmf1, rmf2, dset = make_rmf2()
+    ui.set_data(1, dset)
+    ui.set_rmf(id=1, rmf=rmf1, resp_id=1)
+    ui.set_rmf(id=1, rmf=rmf2, resp_id=2)
+
+    check_eval_multi_rmf()
+
+
+def test_eval_multi_rmf_reorder(clean_astro_ui):
+    """Change the order of setting the RMFs
+
+    Should be the same as test_eval_multi_rmf
+    """
+
+    rmf1, rmf2, dset = make_rmf2()
+    ui.set_data(1, dset)
+    ui.set_rmf(id=1, rmf=rmf2, resp_id=2)
+    ui.set_rmf(id=1, rmf=rmf1, resp_id=1)
+
+    check_eval_multi_rmf()
