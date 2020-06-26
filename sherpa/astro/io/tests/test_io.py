a48f7cd57a7dbf942d93d149ddf87284673915a2
Moritz Guenther <moritz.guenther@gmx.de>
2020-06-26 12:50:39 -0400

    Convert tests to pytest

--- sherpa/astro/io/tests/test_io.py
+++ sherpa/astro/io/tests/test_io.py
@@ -1,5 +1,5 @@
 #
-#  Copyright (C) 2016, 2017, 2018  Smithsonian Astrophysical Observatory
+#  Copyright (C) 2016, 2017, 2018, 2020  Smithsonian Astrophysical Observatory
 #
 #
 #  This program is free software; you can redistribute it and/or modify
@@ -16,41 +16,43 @@
 #  with this program; if not, write to the Free Software Foundation, Inc.,
 #  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
+import pytest
 
-from sherpa.utils.testing import SherpaTestCase, requires_data, \
-    requires_fits, requires_xspec
+import numpy as np
+from sherpa.utils.testing import requires_data, requires_fits, requires_xspec
 from sherpa.astro import ui
 
 from tempfile import NamedTemporaryFile
 
+@pytest.fixture
+def setup(make_data_path):
+    ui.clean()
 
-class test_89_issues(SherpaTestCase):
-    def setUp(self):
-        ui.clean()
 
-    @requires_data
-    @requires_fits
-    @requires_xspec
-    def test_mod_fits(self):
-        tablemodelfile = self.make_path("xspec-tablemodel-RCS.mod")
-        ui.load_table_model("tmod", tablemodelfile)
-        tmod = ui.get_model_component("tmod")
-        self.assertEqual("xstablemodel.tmod", tmod.name)
+@requires_data
+@requires_fits
+@requires_xspec
+def test_mod_fits(make_data_path):
+    tablemodelfile = make_data_path("xspec-tablemodel-RCS.mod")
+    ui.load_table_model("tmod", tablemodelfile)
+    tmod = ui.get_model_component("tmod")
+    assert tmod.name == "xstablemodel.tmod"
 
-    @requires_fits
-    def test_warnings_are_gone_arrays(self):
-        ui.load_arrays(1, [1, 2, 3], [4, 5, 6])
-        #  We now have logic in conftest.py to catch white-listed warnings and fail on unexpected ones.
-        #  We just need to make any warnings bubble up, here and in the following test.
-        with NamedTemporaryFile() as f:
-            ui.save_data(1, f.name, ascii=True, clobber=True)
-        with NamedTemporaryFile() as f:
-            ui.save_data(1, f.name, ascii=False, clobber=True)
+@requires_fits
+def test_warnings_are_gone_arrays():
+    ui.load_arrays(1, [1, 2, 3], [4, 5, 6])
+    #  We now have logic in conftest.py to catch white-listed warnings and fail on unexpected ones.
+    #  We just need to make any warnings bubble up, here and in the following test.
+    with NamedTemporaryFile() as f:
+        ui.save_data(1, f.name, ascii=True, clobber=True)
+    with NamedTemporaryFile() as f:
+        ui.save_data(1, f.name, ascii=False, clobber=True)
 
-    @requires_fits
-    @requires_data
-    def test_warnings_are_gone_pha(self):
-        pha = self.make_path("3c273.pi")
-        ui.load_pha(pha)
-        with NamedTemporaryFile() as f:
-            ui.save_data(1, f.name, ascii=False, clobber=True)
+@requires_fits
+@requires_data
+def test_warnings_are_gone_pha(make_data_path):
+    pha = make_data_path("3c273.pi")
+    ui.load_pha(pha)
+    with NamedTemporaryFile() as f:
+        ui.save_data(1, f.name, ascii=False, clobber=True)
+
