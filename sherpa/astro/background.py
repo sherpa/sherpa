1b008132008ce914421c6c42a2e64a9095329af4
Douglas Burke <dburke.gw@gmail.com>
2020-07-31 14:37:46 -0400

    Remove sherpa.astro.background.BackgroundSumModel
    As we no longer use this model class I have removed it.

--- sherpa/astro/background.py
+++ sherpa/astro/background.py
@@ -22,58 +22,11 @@
 """
 
 from sherpa.models.parameter import Parameter
-from sherpa.models.model import ArithmeticModel, CompositeModel, \
-    modelCacher1d
+from sherpa.models.model import ArithmeticModel, modelCacher1d
 
-from sherpa.utils.err import DataErr, ModelErr
+from sherpa.utils.err import ModelErr
 
-__all__ = ('BackgroundSumModel', 'ScaleArray')
-
-
-class BackgroundSumModel(CompositeModel, ArithmeticModel):
-    """Combine multiple background datasets.
-
-    Define the model expression to be applied to the source
-    region accounting for the background models.
-
-    Parameters
-    ----------
-    srcdata : sherpa.astro.data.DataPHA instance
-        The source dataset.
-    bkgmodels : dict
-        The background components, where the key is the identifier
-        in this dataset and the value is the model. This dictionary
-        cannot be empty.
-
-    """
-
-    def __init__(self, srcdata, bkgmodels):
-        self.srcdata = srcdata
-        self.bkgmodels = bkgmodels
-        scale_factor = self.srcdata.sum_background_data(lambda key, bkg:1)
-        bkgnames = [model.name for model in bkgmodels.values()]
-        name = '%g * (' % scale_factor + ' + '.join(bkgnames) + ')'
-        CompositeModel.__init__(self, name, self.bkgmodels.values())
-
-    def calc(self, p, *args, **kwargs):
-        def eval_bkg_model(key, bkg):
-            bmodel = self.bkgmodels.get(key)
-            if bmodel is None:
-                raise DataErr('bkgmodel', key)
-            # FIXME: we're not using p here (and therefore assuming that the
-            # parameter values have already been updated to match the contents
-            # of p)
-            return bmodel(*args, **kwargs)
-
-        # Evaluate the background model for each dataset using the same
-        # grid and apply the background-to-source correction factors.
-        #
-        # This will only work if the scaling factors are scalars,
-        # since if there are any array elements then the models
-        # have been evaluated on the energy/wavelength grid,
-        # but the correction factors are defined in channel space.
-        #
-        return self.srcdata.sum_background_data(eval_bkg_model)
+__all__ = ('ScaleArray', )
 
 
 class ScaleArray(ArithmeticModel):
