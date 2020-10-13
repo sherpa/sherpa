0666aa51ee1058965cdcdb321d2ae60cf7326f45
wmclaugh <wmclaugh@cfa.harvard.edu>
2020-10-13 12:42:25 -0400

    Add PR#968 to CAT5.2

--- sherpa/astro/utils/__init__.py
+++ sherpa/astro/utils/__init__.py
@@ -19,13 +19,15 @@ from __future__ import absolute_import
 #
 
 import logging
+
 import numpy
-from ._utils import arf_fold, do_group, expand_grouped_mask, \
-    filter_resp, is_in, resp_init, rmf_fold, shrink_effarea
-from ._pileup import apply_pileup
 
 from sherpa.utils import SherpaFloat, get_position, filter_bins
 from sherpa.utils.err import IOErr, DataErr
+
+from ._utils import arf_fold, do_group, expand_grouped_mask, \
+    filter_resp, is_in, resp_init, rmf_fold, shrink_effarea
+from ._pileup import apply_pileup
 
 
 __all__ = ['arf_fold', 'rmf_fold', 'do_group', 'apply_pileup',
@@ -41,16 +43,8 @@ __all__ = ['arf_fold', 'rmf_fold', 'do_group', 'apply_pileup',
 warning = logging.getLogger(__name__).warning
 
 
-try:
-    from ._region import Region, region_mask
-
-    __all__.append('region_mask')
-    __all__.append('Region')
-
-except ImportError:
-    warning('failed to import sherpa.astro.utils._region; Region routines ' +
-            'will not be available')
-
+# Useful constants
+#
 _hc = 12.39841874  # nist.gov in [keV-Angstrom]
 
 
