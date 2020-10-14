e549902d727c631459d3058695420f1544f39b58
Douglas Burke <dburke.gw@gmail.com>
2020-10-13 22:21:06 -0400

    Fix whitespace from #969
    pylint complained about this.

--- sherpa/utils/__init__.py
+++ sherpa/utils/__init__.py
@@ -70,7 +70,7 @@ try:
     multiprocessing_start_method = config.get('multiprocessing', 'multiprocessing_start_method', fallback='fork')
 
     if multiprocessing_start_method not in ('fork', 'spawn', 'default'):
-       raise ValueError('multiprocessing_start method must be one of "fork", "spawn", or "default"')
+        raise ValueError('multiprocessing_start method must be one of "fork", "spawn", or "default"')
 
     if multiprocessing_start_method != 'default':
         multiprocessing.set_start_method(multiprocessing_start_method, force=True)
