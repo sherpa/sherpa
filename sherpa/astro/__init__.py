7eccbddc6c195310bf14ab595a6681012fac5f81
hamogu <moritz.guenther@gmx.de>
2021-06-03 12:12:20 -0400

    Add docs for hc and charge_e

--- sherpa/astro/__init__.py
+++ sherpa/astro/__init__.py
@@ -16,6 +16,14 @@
 #  with this program; if not, write to the Free Software Foundation, Inc.,
 #  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
+'''Physical constants for astronomical spectra
+
+This module contains some physical constants needed to perform conversions
+that are common in the `sherpa.astro` module, e.g. from energy to wavelength.
+'''
 
 hc = 12.39841874  # nist.gov in [keV-Angstrom]
-charge_e = 1.60217653e-09  # elementary charge [ergs] 1 keV, nist.gov
+"""The product of Plank constant (h) and speed of light (c) using values from nist.gov in units appropriate for converting between keV and Angstroms."""
+
+charge_e = 1.60217653e-09
+'''elementary charge from nist.gov in units appropriate for converting between keV and erg.'''
