22f2d447dfaefdf33f0b527db6bc6498b1baa9d5
Dan Nguyen <dnguyen@cfa.harvard.edu>
2021-06-15 14:50:09 -0400

    forgot to update copyright

--- sherpa/astro/ui/tests/test_serialize.py
+++ sherpa/astro/ui/tests/test_serialize.py
@@ -1,5 +1,6 @@
 #
-#  Copyright (C) 2015, 2016, 2018, 2019  Smithsonian Astrophysical Observatory
+#  Copyright (C) 2015, 2016, 2018, 2019, 2021
+#    Smithsonian Astrophysical Observatory
 #
 #
 #  This program is free software; you can redistribute it and/or modify
