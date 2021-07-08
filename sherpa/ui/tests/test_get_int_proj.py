9d250397b949402aa1a0dc25bc7585aab1d46e74
Douglas Burke <dburke.gw@gmail.com>
2021-07-08 18:49:25 -0400

    Tests: remove SherpaTestCase from test_get_int_proj
    As well as removing the UnitTest structure we take advantage of
    pytest features and fixtures. There should be no significant
    changes (other than tweaking the tolerance from 10^-3 to 10^-4).

--- sherpa/ui/tests/test_get_int_proj.py
+++ sherpa/ui/tests/test_get_int_proj.py
@@ -17,85 +17,73 @@
 #  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
 
-import logging
+import pytest
 
 import numpy as np
 
-import pytest
-
-from sherpa.utils.testing import SherpaTestCase
 from sherpa import ui
 
-logger = logging.getLogger('sherpa')
+
+def cmp_args(result):
+    assert result.min == -5
+    assert result.max == 0
+    assert result.nloop == 21
+    assert result.delv is None
+    assert result.fac == 1
+    assert result.log is False
 
 
-class test_get_int_proj(SherpaTestCase):
+def cmp_x(result):
 
-    _get_int_uncproj_recalc_bench = {
-        'x': np.array([-5., -4.75, -4.5, -4.25, -4., -3.75, -3.5, -3.25, -3.,
-                       -2.75, -2.5, -2.25, -2., -1.75, -1.5, -1.25, -1., -0.75,
-                       -0.5, -0.25, 0.]),
-        'proj': np.array([110.0185, 90.493, 72.9534, 57.3995, 43.8316,
-                          32.2494, 22.6531, 15.0427, 9.4181, 5.7794, 4.1265,
-                          4.4594, 6.7782, 11.0829, 17.3734, 25.6497, 35.9119,
-                          48.1599, 62.3938, 78.6135, 96.8191]),
-        'unc': np.array([110.774, 91.1094, 73.4447, 57.78, 44.1153, 32.4507,
+    expected = np.array([-5., -4.75, -4.5, -4.25, -4., -3.75, -3.5, -3.25, -3.,
+                         -2.75, -2.5, -2.25, -2., -1.75, -1.5, -1.25, -1., -0.75,
+                         -0.5, -0.25, 0.])
+    assert result.x == pytest.approx(expected)
+
+
+def cmp_proj(result):
+
+    expected = np.array([110.0185, 90.493, 72.9534, 57.3995, 43.8316,
+                         32.2494, 22.6531, 15.0427, 9.4181, 5.7794, 4.1265,
+                         4.4594, 6.7782, 11.0829, 17.3734, 25.6497, 35.9119,
+                         48.1599, 62.3938, 78.6135, 96.8191])
+    assert result.y == pytest.approx(expected, abs=1.0e-4)
+
+
+
+def cmp_unc(result):
+
+    expected = np.array([110.774, 91.1094, 73.4447, 57.78, 44.1153, 32.4507,
                          22.786, 15.1213, 9.4566, 5.7919, 4.1273, 4.4626,
                          6.7979, 11.1332, 17.4686, 25.8039, 36.1392, 48.4745,
-                         62.8099, 79.1452, 97.4805]),
-        'min': -5,
-        'max': 0,
-        'nloop': 21,
-        'delv': None,
-        'fac': 1,
-        'log': False
-    }
+                         62.8099, 79.1452, 97.4805])
+    assert result.y == pytest.approx(expected, abs=1.0e-4)
 
-    def setUp(self):
-        # defensive programming (one of the tests has been seen to fail
-        # when the whole test suite is run without this)
-        ui.clean()
-        self._old_logger_level = logger.getEffectiveLevel()
-        logger.setLevel(logging.ERROR)
 
-        x = [-13, -5, -3, 2, 7, 12]
-        y = [102.3, 16.7, -0.6, -6.7, -9.9, 33.2]
-        dy = np.ones(6) * 5
-        ui.load_arrays(1, x, y, dy)
-        ui.set_source(ui.polynom1d.poly)
-        poly.c1.thaw()
-        poly.c2.thaw()
-        ui.int_proj(poly.c0)
-        ui.fit()
 
-    def tearDown(self):
-        ui.clean()
+@pytest.fixture
+def setUp(clean_ui, hide_logging):
 
-        try:
-            logger.setLevel(self._old_logger_level)
-        except AttributeError:
-            pass
+    x = [-13, -5, -3, 2, 7, 12]
+    y = [102.3, 16.7, -0.6, -6.7, -9.9, 33.2]
+    dy = np.ones(6) * 5
+    ui.load_arrays(1, x, y, dy)
+    ui.set_source(ui.polynom1d.poly)
+    poly.c1.thaw()
+    poly.c2.thaw()
+    ui.int_proj(poly.c0)
+    ui.fit()
 
-    def cmp_values(self, arg1, arg2):
-        for a, b in zip(arg1, arg2):
-            assert a == pytest.approx(b, abs=1.0e-3)
 
-    def cmp_args(self, result):
-        assert self._get_int_uncproj_recalc_bench["min"] == result.min
-        assert self._get_int_uncproj_recalc_bench["max"] == result.max
-        assert self._get_int_uncproj_recalc_bench["nloop"] == result.nloop
-        assert self._get_int_uncproj_recalc_bench["delv"] == result.delv
-        assert self._get_int_uncproj_recalc_bench["fac"] == result.fac
-        assert self._get_int_uncproj_recalc_bench["log"] == result.log
+def test_get_int_proj(setUp):
+    result = ui.get_int_proj(poly.c1, recalc=True, min=-5, max=0, nloop=21)
+    cmp_args(result)
+    cmp_x(result)
+    cmp_proj(result)
 
-    def test_get_int_proj(self):
-        result = ui.get_int_proj(poly.c1, recalc=True, min=-5, max=0, nloop=21)
-        self.cmp_args(result)
-        self.cmp_values(self._get_int_uncproj_recalc_bench["x"], result.x)
-        self.cmp_values(self._get_int_uncproj_recalc_bench["proj"], result.y)
 
-    def test_get_int_unc(self):
-        result = ui.get_int_unc(poly.c1, recalc=True, min=-5, max=0, nloop=21)
-        self.cmp_args(result)
-        self.cmp_values(self._get_int_uncproj_recalc_bench["x"], result.x)
-        self.cmp_values(self._get_int_uncproj_recalc_bench["unc"], result.y)
+def test_get_int_unc(setUp):
+    result = ui.get_int_unc(poly.c1, recalc=True, min=-5, max=0, nloop=21)
+    cmp_args(result)
+    cmp_x(result)
+    cmp_unc(result)
