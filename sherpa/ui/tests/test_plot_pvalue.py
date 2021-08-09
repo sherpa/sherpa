ba89adde85b2fa80af83242f0608d1e71089a0be
Douglas Burke <dburke.gw@gmail.com>
2021-08-09 14:22:42 -0400

    Tests: clean up test_plot_pvalue
    Minor clean up of the test code (use default fixture for logging,
    avoid double set_source call, note that there's a disconnect in
    the test directory and code).
    
    Also extends the test to check that the nums argument is being
    recognized. This was added after I realized we are not passing
    the bins argument correctly (see forthcoming commit), but it
    turns out that we don't use this argument anyway.

--- sherpa/ui/tests/test_plot_pvalue.py
+++ sherpa/ui/tests/test_plot_pvalue.py
@@ -17,7 +17,9 @@
 #  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
 
-import logging
+"""This is labelled ui but really tests astro/ui
+
+"""
 
 import pytest
 
@@ -26,24 +28,12 @@ from sherpa.utils.testing import requires_data, \
 
 from sherpa.astro import ui
 
-logger = logging.getLogger("sherpa")
-
-
-@pytest.fixture
-def hide_log_output():
-
-    olvl = logger.getEffectiveLevel()
-    logger.setLevel(logging.ERROR)
-
-    yield
-
-    logger.setLevel(olvl)
-
 
 @requires_xspec
 @requires_fits
 @requires_data
-def test_plot_pvalue(make_data_path, clean_astro_ui, hide_log_output):
+def test_plot_pvalue(make_data_path, clean_astro_ui, hide_logging):
+    """Check plot_pvalue with PHA data."""
 
     fname = make_data_path("qso.pi")
     ui.load_pha(fname)
@@ -54,8 +44,7 @@ def test_plot_pvalue(make_data_path, clean_astro_ui, hide_log_output):
     ui.group_counts(10)
     ui.notice(0.3, 8)
 
-    ui.set_model("xsphabs.abs1*xspowerlaw.p1")
-    ui.set_model("abs1*(p1+gauss1d.g1)")
+    ui.set_model("xsphabs.abs1*(xspowerlaw.p1 +gauss1d.g1)")
 
     # move the fit close to the best fit to save a small amount
     # of time.
@@ -69,6 +58,10 @@ def test_plot_pvalue(make_data_path, clean_astro_ui, hide_log_output):
     g1.fwhm = 0.1
     ui.freeze(g1.fwhm)
 
+    # Could we reduce the number of bins to save evaluation time?
+    # We do want a non-default num value when checking the shapes
+    # of the output attributes.
+    #
     ui.fit()
     ui.plot_pvalue(p1, p1 + g1, num=100)
 
@@ -77,3 +70,12 @@ def test_plot_pvalue(make_data_path, clean_astro_ui, hide_log_output):
     assert tmp.null == pytest.approx(210.34566845619273)
     assert tmp.alt == pytest.approx(207.66618095925094)
     assert tmp.lr == pytest.approx(2.679487496941789)
+
+    # Have we returned the correct info?
+    #
+    # Is it worth checking the stored data (aka how randomised is this
+    # output)?
+    #
+    assert tmp.samples.shape == (100, 2)
+    assert tmp.stats.shape == (100, 2)
+    assert tmp.ratios.shape == (100, )
