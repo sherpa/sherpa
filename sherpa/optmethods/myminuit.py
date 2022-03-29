07ba9608b2b64e1ef948395ce04820e0d006cdbc
Douglas Burke <dburke.gw@gmail.com>
2022-03-29 14:49:33 -0400

    myminuit: add copyright

--- sherpa/optmethods/myminuit.py
+++ sherpa/optmethods/myminuit.py
@@ -1,4 +1,23 @@
 #
+#  Copyright (C) 2022
+#  Smithsonian Astrophysical Observatory
+#
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License along
+#  with this program; if not, write to the Free Software Foundation, Inc.,
+#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+#
 # https://github.com/scikit-hep/iminuit
 #
 import numpy
@@ -24,7 +43,7 @@ def myminuit(fcn, x0, xmin, xmax, tol=EPSILON, maxfev=None, leastsqr=True,
     myhugeval = hugeval / 10.0
     for xxx, yyy in zip(xmin, xmax):
         x_tmp = xxx
-        # +/- inf to preven iminuit from transforming
+        # +/- inf to prevent iminuit from transforming
         if xxx < - myhugeval:
             x_tmp = - numpy.inf
         y_tmp = yyy
