b492d9e9d57de81ff2dba231e787a4243c0932fd
Douglas Burke <dburke.gw@gmail.com>
2022-05-01 09:14:29 -0400

    xspec: use our own version comparison rather than LooseVersion
    The LooseVersion class from distutils is deprecated. The suggested
    replacement is packaging.version.Version, which would be an
    easy change, but it would require the packaging package to be
    available when Sherpa is run. As our version-comparison needs are
    very limited we can just use a tuple of integers as the version,
    as that has the semantics we need (e.g. we do not need the full
    PEP 440 support packaging.version.Version provides).

--- sherpa/astro/xspec/utils.py
+++ sherpa/astro/xspec/utils.py
@@ -1,5 +1,6 @@
 #
-#  Copyright (C) 2017, 2018, 2019  Smithsonian Astrophysical Observatory
+#  Copyright (C) 2017, 2018, 2019, 2022
+#  Smithsonian Astrophysical Observatory
 #
 #
 #  This program is free software; you can redistribute it and/or modify
@@ -16,13 +17,45 @@
 #  with this program; if not, write to the Free Software Foundation, Inc.,
 #  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
-from distutils.version import LooseVersion
+
+import re
 
 from . import _xspec
 
 __all__ = ['ModelMeta', 'include_if', 'version_at_least']
 
-XSPEC_VERSION = LooseVersion(_xspec.get_xsversion())
+
+def get_version(version):
+    """Strip out any XSPEC patch level.
+
+    So '12.12.0c' gets converted to '12.12.0', and then to (12, 12,
+    0). This is helpful as then it makes version comparison easier, as
+    we can rely on the standard tuple ordering.
+
+    Parameters
+    ----------
+    version : str
+        The XSPEC version string, of the form "12.12.0c", so it can
+        include the XSPEC patch level.
+
+    Returns
+    -------
+    (major, minor, micro) : tuple of int
+        The XSPEC patchlevel is ignored.
+
+    """
+
+    # XSPEC versions do not match PEP 440, so strip out the trailing
+    # text (which indicates the XSPEC patch level).
+    #
+    match = re.search(r'^(\d+)\.(\d+)\.(\d+)', version)
+    if match is None:
+        raise ValueError(f"Invalid XSPEC version string: {version}")
+
+    return (int(match[1]), int(match[2]), int(match[3]))
+
+
+XSPEC_VERSION = get_version(_xspec.get_xsversion())
 
 
 class ModelMeta(type):
@@ -64,18 +97,29 @@ class ModelMeta(type):
 
 
 def equal_or_greater_than(version_string):
+    """Compare with the version of the current xspec instance.
+
+    Parameters
+    ----------
+    version_string : str
+        The version to compare against the current XSPEC version. It
+        can include an XSPEC patch level, such as "12.12.0c", but the
+        patch level is ignored.
+
+    Returns
+    -------
+    flag : bool
+        `True` if the version of xspec is equal or greater than the
+        argument, `False` otherwise
+
+    Notes
+    -----
+    For better or worse the xspec current instance is not cached
+    across calls. It probably could be but it just seems safer not to,
+    and any overhead insists on models initialization only.
+
     """
-    Utility function that compares a version string with the version of the current xspec instance.
-
-    For better or worse the xspec current instance is not cached across calls. It probably could be but
-    it just seems safer not to, and any overhead insists on models initialization only.
-
-    The comparison is made in terms of the `distutils.version.LooseVersion` class.
-
-    :param version_string: the version against which to compare the current xspec version
-    :return: `True` if the version of xspec is equal or greater than the argument, `False` otherwise
-    """
-    return XSPEC_VERSION >= LooseVersion(version_string)
+    return XSPEC_VERSION >= get_version(version_string)
 
 
 class include_if():
