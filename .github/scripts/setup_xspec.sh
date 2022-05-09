3589f6b16aec0160182596dc1a76393751a66fba
Douglas Burke <dburke.gw@gmail.com>
2022-05-09 09:51:25 -0400

    CI clean up of XSPEC download script
    There's no need for the sed commands to use the global flag on
    these replacememts as there should be only one per line (in fact,
    one per file).

--- .github/scripts/setup_xspec.sh
+++ .github/scripts/setup_xspec.sh
@@ -38,16 +38,16 @@ esac
 echo "* configuring XSPEC"
 
 # Change build configuration
-sed -i.orig "s/#with-xspec=True/with-xspec=True/g" setup.cfg
-sed -i.orig "s|#xspec_lib_dirs = None|xspec_lib_dirs=${xspec_library_path}|g" setup.cfg
-sed -i.orig "s|#xspec_include_dirs = None|xspec_include_dirs=${xspec_include_path}|g" setup.cfg
-sed -i.orig "s|#xspec_version = 12.9.0|xspec_version = ${xspec_version_string}|g" setup.cfg
+sed -i.orig "s|#with-xspec=True|with-xspec=True|" setup.cfg
+sed -i.orig "s|#xspec_lib_dirs = None|xspec_lib_dirs=${xspec_library_path}|" setup.cfg
+sed -i.orig "s|#xspec_include_dirs = None|xspec_include_dirs=${xspec_include_path}|" setup.cfg
+sed -i.orig "s|#xspec_version = .*|xspec_version = ${xspec_version_string}|" setup.cfg
 
-#Xspec ~12.10.1n now requires fftw. This disables the fftw build from extern
-sed -i.orig "s|#fftw=local|fftw=local|g" setup.cfg
-sed -i.orig "s|#fftw-include_dirs=build/include|fftw-include_dirs=${CONDA_PREFIX}/include|g" setup.cfg
-sed -i.orig "s|#fftw-lib-dirs=build/lib|fftw-lib-dirs=${CONDA_PREFIX}/lib|g" setup.cfg
-sed -i.orig "s|#fftw-libraries=fftw3|fftw-libraries=fftw3|g" setup.cfg
+# Xspec ~12.10.1n now requires fftw. This disables the fftw build from extern
+sed -i.orig "s|#fftw=local|fftw=local|" setup.cfg
+sed -i.orig "s|#fftw-include_dirs=build/include|fftw-include_dirs=${CONDA_PREFIX}/include|" setup.cfg
+sed -i.orig "s|#fftw-lib-dirs=build/lib|fftw-lib-dirs=${CONDA_PREFIX}/lib|" setup.cfg
+sed -i.orig "s|#fftw-libraries=fftw3|fftw-libraries=fftw3|" setup.cfg
 
 echo "* START setup.cfg"
 cat setup.cfg
