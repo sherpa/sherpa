0a366a632abc69e708a92427fbd9d84f8b35c3bc
Douglas Burke <dburke.gw@gmail.com>
2022-05-09 09:29:23 -0400

    CI split the xspec and ds9 setup scripts
    At the moment we treat "having XSPEC" as a synonym for "also
    install DS9". Tehcnically they are separate, so split up the
    download scripts as it makes it easier to see what each script
    does. It would be possible to set up a CI run which only adds
    one of XSPEC and DS9 support, but that is not a high priority.

--- .github/scripts/setup_xspec.sh
+++ .github/scripts/setup_xspec.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash -e
 
-ds9_base_url=https://ds9.si.edu/download/
-
 # variable $CONDA_PREFIX should be defined by conda by using conda activate (in setup_conda.sh)
 if [[ "x${CONDA_PREFIX}" == "x" ]];
 then
@@ -15,51 +13,9 @@ then
     exit 1
 fi
 echo "HEADAS=${HEADAS}"
+
 #Set the xspec_root as the top of the Conda environment
 xspec_root=${CONDA_PREFIX}
-
-if [ "`uname -s`" == "Darwin" ] ; then
-    ds9_os=darwinmojave
-else
-    echo "* installing dev environment"
-
-    # install build dependencies
-    sudo apt-get update
-    sudo apt-get install -qq libx11-dev libsm-dev libxrender-dev
-
-    # set os-specific variables
-    ds9_os=ubuntu18
-fi
-
-echo "* ds9_os=$ds9_os"
-
-download () {
-  echo "* downloading $1"
-  wget --quiet $1
-  if [[ $? -ne 0 ]]; then
-    echo "\n*** Unable to download $1\n"
-  fi
-}
-
-### DS9 and XPA
-# Tarballs to fetch
-ds9_tar=ds9.${ds9_os}.8.3.tar.gz
-xpa_tar=xpa.${ds9_os}.2.1.20.tar.gz
-
-# Fetch them
-download $ds9_base_url/$ds9_os/$ds9_tar
-download  $ds9_base_url/$ds9_os/$xpa_tar
-
-# untar them
-echo "* unpacking ds9/XPA"
-start_dir=$(pwd)
-cd ${CONDA_PREFIX}/bin
-tar xf ${start_dir}/${ds9_tar}
-tar xf ${start_dir}/${xpa_tar}
-cd -
-
-
-### XSPEC
 xspec_library_path=${xspec_root}/lib/
 xspec_include_path=${xspec_root}/include/
 
