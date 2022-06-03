3b88477d96671c303d2b7bb7c28d6f08c72b6c5a
Douglas Burke <dburke.gw@gmail.com>
2022-06-03 14:30:31 -0400

    Tests: remove u-string prefix (Python 3)
    The tests were using unicode strings, but this is not needed with
    Python 3.
    
    I checked and this appears to be the only case of this in the
    code base - checked with
    
        pylint sherpa --disable all --enable redundant-u-string-prefix

--- sherpa/ui/tests/test_session.py
+++ sherpa/ui/tests/test_session.py
@@ -274,7 +274,7 @@ def test_paramprompt():
     # raised, and the text, but then we don't get to test the
     # behaviour of the paramprompt code.
     #
-    with patch("sys.stdin", StringIO(u"2.1")):
+    with patch("sys.stdin", StringIO("2.1")):
         s.set_model('const1d.mx')
 
     assert s.list_model_ids() == [1]
@@ -284,7 +284,7 @@ def test_paramprompt():
     mx = s.get_model_component('mx')
     assert mx.c0.val == pytest.approx(2.1)
 
-    with patch("sys.stdin", StringIO(u"2.1e-3 , 2.0e-3, 1.2e-2")):
+    with patch("sys.stdin", StringIO("2.1e-3 , 2.0e-3, 1.2e-2")):
         s.set_model('x', 'const1d.my')
 
     assert set(s.list_model_ids()) == set([1, 'x'])
@@ -296,7 +296,7 @@ def test_paramprompt():
     assert my.c0.min == pytest.approx(2.0e-3)
     assert my.c0.max == pytest.approx(1.2e-2)
 
-    with patch("sys.stdin", StringIO(u"2.1e-3 ,, 1.2e-2")):
+    with patch("sys.stdin", StringIO("2.1e-3 ,, 1.2e-2")):
         s.set_model(2, 'const1d.mz')
 
     assert set(s.list_model_ids()) == set([1, 2, 'x'])
@@ -308,7 +308,7 @@ def test_paramprompt():
     assert mz.c0.min == pytest.approx(- parameter.hugeval)
     assert mz.c0.max == pytest.approx(1.2e-2)
 
-    with patch("sys.stdin", StringIO(u"-12.1,-12.1")):
+    with patch("sys.stdin", StringIO("-12.1,-12.1")):
         s.set_model('const1d.f1')
 
     assert set(s.list_model_ids()) == set([1, 2, 'x'])
@@ -320,7 +320,7 @@ def test_paramprompt():
     assert f1.c0.min == pytest.approx(-12.1)
     assert f1.c0.max == pytest.approx(parameter.hugeval)
 
-    with patch("sys.stdin", StringIO(u" ,-12.1")):
+    with patch("sys.stdin", StringIO(" ,-12.1")):
         s.set_model('const1d.f2')
 
     # stop checking list_model_ids
@@ -332,7 +332,7 @@ def test_paramprompt():
     assert f2.c0.min == pytest.approx(-12.1)
     assert f2.c0.max == pytest.approx(parameter.hugeval)
 
-    with patch("sys.stdin", StringIO(u" ,")):
+    with patch("sys.stdin", StringIO(" ,")):
         s.set_model('const1d.f3')
 
     f3 = s.get_model_component('f3')
@@ -340,7 +340,7 @@ def test_paramprompt():
     assert f3.c0.min == pytest.approx(- parameter.hugeval)
     assert f3.c0.max == pytest.approx(parameter.hugeval)
 
-    with patch("sys.stdin", StringIO(u" ,, ")):
+    with patch("sys.stdin", StringIO(" ,, ")):
         s.set_model('const1d.f4')
 
     f4 = s.get_model_component('f4')
