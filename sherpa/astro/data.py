725c6dd741efa1e9d72bccde7b70e671440c4415
Douglas Burke <dburke.gw@gmail.com>
2024-01-03 09:04:28 -0500

    Support multi-matrix RMF
    Provide initial support for multi-matrix RMF. There are basically
    two approaches given the current structure
    
    - create multiple DataRMF files and apply them individually
    - create a single RMF instance that applies them all at "once"
    
    The former would perhaps fit into the existing code, since we have
    the existing sherpa.astro.instrument.MultipleResponse1D class, but
    
    a) I don't really understand the trade offs that has
    b) the existing code deals with multiple files, where it makes
       sense to incorporate them, whereas the multi-matrix case
       has a single file
    
    So I have gone for the second option as an experiment. This also
    *technically* has the advantage that we can make this more efficient
    as we just call the rmf fold once, and there can take advantage of
    knowledge of the response energy grid. However, as discussed in
    issue #1907, we don't have a good way of associating the evaluated
    model with the energy grid used to create it. So we need to work
    on this (which will probably lead to better understanding the
    MultiResponse1D class as well).
    
    There is also the fact that the DataRMF class has special case
    for filtering the data based on the noticed channels in a PHA
    which we currently overwrite/avoid in the multi-matrix case. This
    is again because I do not understand this code.
    
    At present we can not write out multi-matrix RMF files - we just
    write out the "highest resolution" case.

--- sherpa/astro/data.py
+++ sherpa/astro/data.py
@@ -120,15 +120,15 @@ this range to have at least 20 counts per group:
 
 from __future__ import annotations
 
+from dataclasses import dataclass
 import logging
 import os
-from typing import Any, Callable, Literal, Mapping, Optional, Sequence, \
-    Union, cast, overload
+from typing import TYPE_CHECKING, Any, Callable, Literal, Mapping, \
+    Optional, Sequence, Union, cast, overload
 import warnings
 
 import numpy as np
 
-from sherpa.astro import hc
 from sherpa.data import Data1DInt, Data2D, Data, Data1D, \
     IntegratedDataSpace2D, _check
 from sherpa.models.regrid import EvaluationSpace1D
@@ -141,10 +141,16 @@ from sherpa.utils import formatting
 from sherpa.utils.numeric_types import SherpaFloat
 from sherpa.utils.types import ArrayType, IdType, ModelFunc, StatErrFunc
 
+from . import hc
+
 # There are currently (Sep 2015) no tests that exercise the code that
 # uses the compile_energy_grid symbols.
-from sherpa.astro.utils import arf_fold, rmf_fold, filter_resp, \
+from .utils import arf_fold, rmf_fold, filter_resp, \
     compile_energy_grid, do_group, expand_grouped_mask
+
+if TYPE_CHECKING:
+    # Avoid reference loops
+    from .io.types import KeyType
 
 info = logging.getLogger(__name__).info
 warning = logging.getLogger(__name__).warning
@@ -1240,6 +1246,142 @@ class DataRosatRMF(DataRMF):
 
     def _validate(self, name, energy_lo, energy_hi, ethresh):
         return energy_lo, energy_hi
+
+
+@dataclass
+class MatrixRMF:
+    """Store the data for the RMF MATRIX block.
+
+    There is no validation of these fields as this is intended
+    for internal use only.
+    """
+
+    energ_lo: np.ndarray
+    energ_hi: np.ndarray
+    n_grp: np.ndarray
+    f_chan: np.ndarray
+    n_chan: np.ndarray
+    matrix: np.ndarray
+
+
+# Using a single class makes sense given the existing API but there
+# are reasons why we might want the logic in a sherpa.astro.instrument
+# class instead. This is therefore an experiment as we explore
+# supporting this type of data.
+#
+class DataMultiRMF(DataRMF):
+    """Support multi-matrix RMF.
+
+    Allow a multi-block RMF file - where the matrix information is
+    stored at different energy binning - as a `DataRMF` object. This
+    is an *experimental* interface which may change.
+
+    Parameters
+    ----------
+    name : str
+        The name of the data set; often set to the name of the file
+        containing the data.
+    detchans : int
+    matrices : list of MatrixMF
+        The matrix information.
+    e_min, e_max : array-like
+    offset : int, optional
+    header : dict or None, optional
+    ethresh : number or None, optional
+        If set it must be greater than 0 and is the replacement value
+        to use if the lowest-energy value is 0.0.
+
+    Notes
+    -----
+    The current representations of this RMF (e.g. string
+    representation and the rich visualization in notebooks) use the
+    "highest resolution" matrix only.
+
+    """
+
+    def __init__(self,
+                 name: str,
+                 detchans: int,
+                 matrices: list[MatrixRMF],
+                 e_min: np.ndarray,
+                 e_max: np.ndarray,
+                 offset: int = 1,
+                 header: Optional[Mapping[str, KeyType]] = None,
+                 ethresh: Optional[float] = None
+                 ) -> None:
+
+        nmat = len(matrices)
+        if nmat < 2:
+            raise ValueError(f"expected at least 2 matrices, sent {nmat}")
+
+        self._matrices: list[MatrixRMF] = []
+        for mat in matrices:
+            self._matrices.append(mat)
+
+        # Does it really matter what we send to DataRMF here?  For now
+        # pick the matix with the most energy bins, as the assumption
+        # is that this is the "high-res" response, but there's nothing
+        # in the spec to stop "strange" RMFs being created.
+        #
+        # We could instead go and combine the different energ_lo/hi
+        # bins to create an overall grid, and then resample that for
+        # each matrix.
+        #
+        nbins = np.asarray([len(mat.energ_lo)
+                            for mat in matrices])
+        idx = np.argmax(nbins)
+        mat = matrices[idx]
+
+        # Store the identity of the "highest-res" matrix.
+        self._matrix0 = idx
+
+        super().__init__(name, detchans, mat.energ_lo,
+                         mat.energ_hi, mat.n_grp, mat.f_chan,
+                         mat.n_chan, mat.matrix, offset=offset,
+                         e_min=e_min, e_max=e_max, header=header,
+                         ethresh=ethresh)
+
+    # For now we drop all the fancy "restrict to just the range
+    # we care about".
+    #
+    def notice(self, noticed_chans=None):
+        return None
+
+    def apply_rmf(self, src, *args, **kwargs):
+        """Apply the RMF to the source model.
+        """
+
+        # We need to fold the model through each RMF and sum the
+        # results. Note that the use of uint64 for detchans can make
+        # the size calculation overflow into a float, hence the int
+        # call.
+        #
+        out = np.zeros(int(self.detchans + 1 - self.offset),
+                       dtype=SherpaFloat)
+        for mat in self._matrices:
+            elo = mat.energ_lo
+            ehi = mat.energ_hi
+
+            # The API should include the evaluation space for the
+            # model, as that would save a lot of issues here. As
+            # mentioned in #1907 this information may be sent
+            # here, bit it's not currently guaranteed.
+            #
+            # Should args[0] be checked, as this should be the grid
+            # used to evaluate src (but can we guarantee that)?
+            #
+            if len(src) == len(elo):
+                # This is not really a sufficient check.
+                #
+                tmp_src = src
+            else:
+                tmp_src = rebin(src, self._lo, self._hi, elo, ehi)
+
+            out += rmf_fold(tmp_src, mat.n_grp, mat.f_chan,
+                            mat.n_chan, mat.matrix, self.detchans,
+                            self.offset)
+
+        return out
 
 
 def validate_wavelength_limits(wlo, whi, emax):
