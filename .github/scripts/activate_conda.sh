09b4f485d2d33429de1341316f6d18117acd2e4a
Doug Burke <dburke@cfa.harvard.edu>
2024-04-26 10:38:29 -0400

    CI: install miniforge on macOS
    The macOS arm builds do not include conda, so install miniforge
    in this case.
    
    Note that we do not provide a macOS arm conda package for
    xspec-modelsonly at this time, so we take the package from the
    CIAO build in this situation. This is hopefully a temporary
    solution.

--- .github/scripts/activate_conda.sh
+++ .github/scripts/activate_conda.sh
@@ -2,5 +2,10 @@
 
 # Activate conda and select the environment
 
+if [ -z "${CONDA}" ]; then
+    # set up for macOS
+    CONDA="${HOME}/conda"
+fi
+
 source ${CONDA}/etc/profile.d/conda.sh
 conda activate build
