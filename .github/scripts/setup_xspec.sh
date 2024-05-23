df448ed753f6129eb5594a02c25de29e07a49816
Marie-Terrell <marie.terrell@cfa.harvard.edu>
2024-05-23 10:19:34 -0400

    Bump CI xspec versions to include 12.14.0h

--- .github/scripts/setup_xspec.sh
+++ .github/scripts/setup_xspec.sh
@@ -20,17 +20,8 @@ xspec_library_path=${xspec_root}/lib/
 xspec_include_path=${xspec_root}/include/
 
 case "${XSPECVER}" in
-  12.13.1*)
-      xspec_version_string="12.13.1"
-      ;;
-  12.13.0*)
-      xspec_version_string="12.13.0"
-      ;;
-  12.12.1*)
-      xspec_version_string="12.12.1"
-      ;;
-  12.12.0*)
-      xspec_version_string="12.12.0"
+  12.1*.* )
+      xspec_version_string="${XSPECVER:0:7}"
       ;;
   *)
       echo "Xspec version listed currently unsupported in GitHub Actions jobs."
