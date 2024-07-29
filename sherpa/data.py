95ed0c89282c4659fa0d05854e2ae77414755123
Douglas Burke <dburke.gw@gmail.com>
2024-07-29 09:18:30 -0400

    data: try to improve documentation for Data1DAsymmetricErrs
    More is needed but I am not the person to write it.

--- sherpa/data.py
+++ sherpa/data.py
@@ -1998,7 +1998,20 @@ class Data1D(Data):
 class Data1DAsymmetricErrs(Data1D):
     """1-D data set with asymmetric errors
 
-    Note: elo and ehi shall be stored as delta values from y
+    See Also
+    --------
+    sherpa.sim.ReSampleData
+
+    Notes
+    -----
+    The elo and ehi fields are stored as delta values relative to y.
+
+    Sherpa's data and statistic objects assume symmetric errors, and
+    so the methods here sometimes return a single array for an error,
+    and sometimes a pair or 2D array (giving the low and high values).
+    The assumption is that the staterror field is set when the object
+    is created.
+
     """
 
     _fields: FieldsType = ("name", "x", "y", "staterror", "syserror", "elo", "ehi")
@@ -2021,10 +2034,11 @@ class Data1DAsymmetricErrs(Data1D):
     def get_yerr(self, filter=False, staterrfunc=None):
         """Return the y error.
 
-        The staterrfunc is currently ignored.
+        The staterrfunc argument is currently ignored.
         """
 
         if not filter:
+            # A minor optimization
             return self.elo, self.ehi
 
         return self.apply_filter(self.elo), self.apply_filter(self.ehi)
