fe46934920022e428dad65913218309b12872ef1
Douglas Burke <dburke.gw@gmail.com>
2024-09-04 11:51:26 -0400

    UI: add calc_model/source commands
    If you want to evaluate a model to match the data then the
    easiest way has been to call get_model_plot and then use the
    y attribute of the plot object. However, this is annoying
    because users do not always want to use the plt options - e.g.
    normalize by the exposure time or divide by the bin width.
    
    So, we add
    
        calc_model()
        calc_source()
    
    routines which evaluate the model on the relevant grid and return
    
    - the independent axis (if integerated will get both edges)
    - the evaluated model value
    
    The return values depend in part on the data object - e.g. the
    analysis setting for PHA data - and in part the model (i.e. what
    are its units).
    
    There is an asymmetry in these two routines, since
    
    - calc_model will filter (and group) the data
    - calc_source does not filter (and can not group)
    
    We could apply a filter to the calc_source data to "match" the
    chosen data filter, but it is not ideal (since notice/ignore act
    on the EBOUNDS energy grid and this is the MATRIX energy grid,
    which is technically different).
    
    An alternative would be to follow plot_source and add lo/hi
    arguments so a user can say "only give me the data between these
    two limits" - but it's easy for a user to do this themselves,
    and it wouldn't reduce the time taken to make the call.

--- sherpa/astro/ui/tests/test_astro_ui_utils_unit.py
+++ sherpa/astro/ui/tests/test_astro_ui_utils_unit.py
@@ -32,8 +32,7 @@ import numpy as np
 
 import pytest
 
-from sherpa.astro import io
-from sherpa.astro import ui
+from sherpa.astro import hc, io, ui
 from sherpa.utils.err import ArgumentErr, ArgumentTypeErr, DataErr, \
     IOErr
 from sherpa.utils.logging import SherpaVerbosity
@@ -396,3 +395,328 @@ def test_show_xsabund(clean_astro_ui, caplog):
     assert txt[9] == ""
 
     assert len(txt) == 10
+
+
+@pytest.mark.parametrize("name", ["model", "source"])
+@pytest.mark.parametrize("arg", [None, 1])
+@pytest.mark.parametrize("empty", [ui.Data1D("x", None, None),
+                                   # At present the code does not handle
+                                   # these cases well (i.e. they error out
+                                   # with an internal error thanks to there
+                                   # being no data).
+                                   #
+                                   # ui.Data1DInt("x", None, None, None),
+                                   # ui.DataPHA("x", None, None)
+                                   ])
+def test_calc_xxx_empty(name, arg, empty, clean_astro_ui):
+    """Does calc_source/model error out if empty?"""
+
+    if arg is None:
+        ui.set_data(empty)
+    else:
+        ui.set_data(arg, empty)
+
+    ui.set_source(1, ui.polynom1d.mdl)
+    mdl.c0 = 2
+    mdl.c1 = 4
+
+    getfunc = getattr(ui, f"calc_{name}")
+    with pytest.raises(DataErr,
+                       match="^The size of '1' has not been set$"):
+        getfunc(arg)
+
+
+@pytest.mark.parametrize("name", ["model", "source"])
+@pytest.mark.parametrize("arg", [None, 1])
+def test_calc_xxx_data1d(name, arg, clean_astro_ui):
+    """Does calc_source/model work?"""
+
+    ui.load_arrays(1, [1, 4, 9], [0, 0, 0])
+    ui.set_source(1, ui.polynom1d.mdl)
+    mdl.c0 = 2
+    mdl.c1 = 4
+
+    x = np.asarray([1, 4, 9])
+
+    getfunc = getattr(ui, f"calc_{name}")
+    xval, yval = getfunc(arg)
+
+    assert len(xval) == 1
+    assert xval[0] == pytest.approx(x)
+
+    # y = 4 x + 2
+    #
+    assert yval == pytest.approx(4 * x + 2)
+
+
+@pytest.mark.parametrize("name", ["model", "source"])
+@pytest.mark.parametrize("arg", [None, 1])
+def test_calc_xxx_data1d_int(name, arg, clean_astro_ui):
+    """Does calc_source/model work?"""
+
+    ui.load_arrays(1, [1, 4, 9], [2, 6, 10], [0, 0, 0], ui.Data1DInt)
+    ui.set_source(1, ui.polynom1d.mdl)
+    mdl.c0 = 2
+    mdl.c1 = 4
+
+    xlo = np.asarray([1, 4, 9])
+    xhi = np.asarray([2, 6, 10])
+
+    getfunc = getattr(ui, f"calc_{name}")
+    xval, yval = getfunc(arg)
+
+    assert len(xval) == 2
+    assert xval[0] == pytest.approx(xlo)
+    assert xval[1] == pytest.approx(xhi)
+
+    # y = 4 x + 2 integrated across each bin
+    # (1,2), (4,6), (9,10)
+    #
+    def integ(a, b):
+        return 2 * (b**2 - a**2) + 2 * (b - a)
+
+    assert yval == pytest.approx(integ(xlo, xhi))
+
+
+# Do not bother with id checks here.
+#
+def test_calc_model_datapha_no_response(clean_astro_ui):
+    """Does calc_model work? DataPHA no response"""
+
+    ui.load_arrays(1, [1, 2, 3], [0, 0, 0], ui.DataPHA)
+    ui.set_source(1, ui.polynom1d.mdl)
+
+    with pytest.raises(DataErr,
+                       match="^No instrument response found for dataset $"):
+        ui.calc_model()
+
+
+def create_test_datapha():
+    """Used in test_calc_model_datapha_xxx"""
+
+    ARFVAL = 12.8
+    EXPVAL = 2.0
+
+    ebins = np.asarray([0.1, 0.2, 0.4, 0.7])
+    elo = ebins[:-1]
+    ehi = ebins[1:]
+    arf = np.full(elo.shape, ARFVAL)
+
+    ui.load_arrays(1, [1, 2, 3], [0, 0, 0], ui.DataPHA)
+    ui.set_exposure(EXPVAL)
+    ui.set_rmf(ui.create_rmf(elo, ehi, e_min=elo, e_max=ehi))
+    ui.set_arf(ui.create_arf(elo, ehi, specresp=arf))
+
+    ui.set_source(1, ui.polynom1d.mdl)
+    mdl.c0 = 2
+    mdl.c1 = 4
+
+    def integ(a, b):
+        return 2 * (b**2 - a**2) + 2 * (b - a)
+
+    return (elo, ehi, integ, ARFVAL * EXPVAL)
+
+
+def test_calc_model_datapha_energy(clean_astro_ui):
+    """Does calc_model work? DataPHA, energy"""
+
+    (elo, ehi, integ, norm) = create_test_datapha()
+
+    assert ui.get_analysis() == "energy"
+
+    xval, yval = ui.calc_model()
+
+    assert len(xval) == 2
+    assert xval[0] == pytest.approx(elo)
+    assert xval[1] == pytest.approx(ehi)
+
+    # y = 4 x + 2 integrated across each bin
+    # (0.1,0.2), (0.2,0.4), (0.4,0.7) and then corrected
+    # for the arf (12.8 for each bin) and exposure time (2).
+    # Could have made the ARF vary per bin but leave that for
+    # other tests.
+    #
+    assert yval == pytest.approx(norm * integ(elo, ehi))
+
+    # Filter the data
+    #
+    ui.ignore(hi=0.15)
+    elo2 = elo[1:]
+    ehi2 = ehi[1:]
+
+    xval2, yval2 = ui.calc_model()
+    assert xval2[0] == pytest.approx(elo2)
+    assert xval2[1] == pytest.approx(ehi2)
+    assert yval2 == pytest.approx(norm * integ(elo2, ehi2))
+
+
+def test_calc_model_datapha_wavelength(clean_astro_ui):
+    """Does calc_model work? DataPHA, wavelength"""
+
+    (elo, ehi, integ, norm) = create_test_datapha()
+
+    ui.set_analysis("wave")
+    assert ui.get_analysis() == "wavelength"
+
+    xval, yval = ui.calc_model()
+
+    wlo = hc / elo
+    whi = hc / ehi
+
+    assert len(xval) == 2
+    assert xval[0] == pytest.approx(wlo)
+    assert xval[1] == pytest.approx(whi)
+
+    # y = 4 x + 2 integrated across each bin
+    # (0.1,0.2), (0.2,0.4), (0.4,0.7) and then corrected
+    # for the arf (12.8 for each bin) and exposure time (2).
+    # Could have made the ARF vary per bin but leave that for
+    # other tests.
+    #
+    # Note this uses whi,wlo to evaluate the model since this
+    # is not an XSPEC model. This is not ideal!
+    # See issue #850
+    #
+    assert yval == pytest.approx(norm * integ(whi, wlo))
+
+    # Filter the data
+    #
+    ui.ignore(hi=20)
+    wlo2 = wlo[:-1]
+    whi2 = whi[:-1]
+
+    xval2, yval2 = ui.calc_model()
+    assert xval2[0] == pytest.approx(wlo2)
+    assert xval2[1] == pytest.approx(whi2)
+    assert yval2 == pytest.approx(norm * integ(whi2, wlo2))
+
+
+def test_calc_model_datapha_channel(clean_astro_ui):
+    """Does calc_model work? DataPHA, channel"""
+
+    (elo, ehi, integ, norm) = create_test_datapha()
+
+    ui.set_analysis("bin")
+    assert ui.get_analysis() == "channel"
+
+    xval, yval = ui.calc_model()
+
+    xlo = np.arange(1, 4)
+    xhi = xlo + 1
+
+    assert len(xval) == 2
+    assert xval[0] == pytest.approx(xlo)
+    assert xval[1] == pytest.approx(xhi)
+
+    # y = 4 x + 2 integrated across each bin
+    # (0.1,0.2), (0.2,0.4), (0.4,0.7) and then corrected
+    # for the arf (12.8 for each bin) and exposure time (2).
+    # Could have made the ARF vary per bin but leave that for
+    # other tests.
+    #
+    # Note that this is not evaluated in channel units!
+    assert yval == pytest.approx(norm * integ(elo, ehi))
+
+    # Filter the data
+    #
+    ui.ignore(hi=1)
+    xlo2 = xlo[1:]
+    xhi2 = xhi[1:]
+
+    xval2, yval2 = ui.calc_model()
+    assert xval2[0] == pytest.approx(xlo2)
+    assert xval2[1] == pytest.approx(xhi2)
+    assert yval2 == pytest.approx(norm * integ(elo[1:], ehi[1:]))
+
+
+def test_calc_source_datapha_energy(clean_astro_ui):
+    """Does calc_source work? DataPHA, energy"""
+
+    (elo, ehi, integ, _) = create_test_datapha()
+
+    assert ui.get_analysis() == "energy"
+
+    xval, yval = ui.calc_source()
+
+    assert len(xval) == 2
+    assert xval[0] == pytest.approx(elo)
+    assert xval[1] == pytest.approx(ehi)
+
+    # Since the response is "perfect" - apart from the arf value and
+    # exposure - we can use the same integration term as for the
+    # calc_model tests.
+    #
+    assert yval == pytest.approx(integ(elo, ehi))
+
+    # Filter the data
+    #
+    ui.ignore(hi=0.15)
+
+    # Does not change calc_source.
+    #
+    xval2, yval2 = ui.calc_source()
+    assert xval2[0] == pytest.approx(elo)
+    assert xval2[1] == pytest.approx(ehi)
+    assert yval2 == pytest.approx(integ(elo, ehi))
+
+
+def test_calc_source_datapha_wavelength(clean_astro_ui):
+    """Does calc_source work? DataPHA, wavelength"""
+
+    (elo, ehi, integ, _) = create_test_datapha()
+
+    ui.set_analysis("wave")
+    assert ui.get_analysis() == "wavelength"
+
+    xval, yval = ui.calc_source()
+
+    # NOTE: this is different than calc_model!
+    wlo = hc / ehi
+    whi = hc / elo
+
+    assert len(xval) == 2
+    assert xval[0] == pytest.approx(wlo)
+    assert xval[1] == pytest.approx(whi)
+
+    assert yval == pytest.approx(integ(wlo, whi))
+
+    # Filter the data
+    #
+    ui.ignore(hi=20)
+
+    # Does not change calc_source.
+    #
+    xval2, yval2 = ui.calc_source()
+    assert xval2[0] == pytest.approx(wlo)
+    assert xval2[1] == pytest.approx(whi)
+    assert yval2 == pytest.approx(integ(wlo, whi))
+
+
+def test_calc_source_datapha_channel(clean_astro_ui):
+    """Does calc_source work? DataPHA, channel"""
+
+    (elo, ehi, integ, _) = create_test_datapha()
+
+    ui.set_analysis("bin")
+    assert ui.get_analysis() == "channel"
+
+    xval, yval = ui.calc_source()
+
+    # X axis is in keV, as channel is not appropriate.
+    #
+    assert len(xval) == 2
+    assert xval[0] == pytest.approx(elo)
+    assert xval[1] == pytest.approx(ehi)
+
+    assert yval == pytest.approx(integ(elo, ehi))
+
+    # Filter the data
+    #
+    ui.ignore(hi=1)
+
+    # Does not change calc_source.
+    #
+    xval2, yval2 = ui.calc_source()
+    assert xval2[0] == pytest.approx(elo)
+    assert xval2[1] == pytest.approx(ehi)
+    assert yval2 == pytest.approx(integ(elo, ehi))
