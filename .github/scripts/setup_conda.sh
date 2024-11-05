5d66edd2fc1740fb1c101181310ec578d86dfdbb
Marie-Terrell <marie.terrell@cfa.harvard.edu>
2024-11-05 09:18:30 -0500

    Test installing Conda ourselves like deployment workflow

--- .github/scripts/setup_conda.sh
+++ .github/scripts/setup_conda.sh
@@ -24,13 +24,7 @@ fi
 
 # Ensure we have set up conda
 #
-source ${CONDA}/etc/profile.d/conda.sh
-
-# update and add channels
-conda update --yes conda
-conda config --add channels conda-forge
-#Remove defaults to avoid conflicts with conda-forge
-conda config --remove channels defaults
+source ${conda_loc}/etc/profile.d/conda.sh
 
 # To avoid issues with non-XSPEC builds (e.g.
 # https://github.com/sherpa/sherpa/pull/794#issuecomment-616570995 )
