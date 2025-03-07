2d99a18250f72c996440ed19dffeb9c0feeb5225
Douglas Burke <dburke.gw@gmail.com>
2025-03-07 09:13:00 -0500

    estmethods: remove old comments
    These comments are either
    
    - not useful (e.g. explaining very-basic error estimation steps)
    - are for unused code that has been sitting around unused for
      a long time

--- sherpa/estmethods/__init__.py
+++ sherpa/estmethods/__init__.py
@@ -1,5 +1,5 @@
 #
-#  Copyright (C) 2007, 2015, 2016, 2019 - 2021, 2023, 2024
+#  Copyright (C) 2007, 2015, 2016, 2019 - 2021, 2023 - 2025
 #  Smithsonian Astrophysical Observatory
 #
 #
@@ -41,6 +41,7 @@ __all__ = ('EstNewMin', 'Covariance', 'Confidence',
            'est_hardmax', 'est_hardminmax', 'est_newmin', 'est_maxiter',
            'est_hitnan')
 
+
 est_success = 0
 est_failure = 1
 est_hardmin = 2
@@ -50,52 +51,10 @@ est_newmin = 5
 est_maxiter = 6
 est_hitnan = 7
 
-# For every method listed here, we have the same goal:  derive confidence
-# limits for thawed parameters.  Thawed parameters are allowed to vary
-# during a fit; when a model has been fit to data, then the current
-# parameter values are presumably the best-fit values.  Think of the
-# best-fit values as being at the lowest point in a valley in parameter
-# space--any step away, in any direction, means a worse fit (i.e., the
-# value of the fit statistic is greater).  Confidence limits tell you
-# how well constrained those best fit values are; i.e., are we in a deep,
-# narrow valley in parameter space?  If so, we can be confident the limits
-# are small.  But if the valley is shallow and broad, then the confidence
-# limits will also be very broad.
-#
-# Every method is passed the same information:
-#  the current values of all thawed parameters;
-#  the soft limits of all thawed parameters;
-#  the hard limits of all thawed parameters;
-#  the list of parameters for which we are actually want confidence
-#    limits (this can be a subset of all thawed parameters)
-#  a reference to the statistic function;
-#  a reference to the fitting function.
 
-
-# class EstMethodError(SherpaError):
-#    "Reached an error while computing parameter confidence limits"
-#    pass
-#
-# class EstHardMin(EstMethodError):
-#    "Reached a parameter hard minimum"
-#    pass
-#
-# class EstHardMax(EstMethodError):
-#    "Reached a parameter hard maximum"
-#    pass
-#
 class EstNewMin(Exception):
-
     "Reached a new minimum fit statistic"
     pass
-#
-# class EstMaxIter(EstMethodError):
-#    "Reached maximum iterations in scaling function"
-#    pass
-#
-# class EstNaN(EstMethodError):
-#    "Reached a NaN during computation"
-#    pass
 
 
 class EstMethod(NoNewAttributesAfterInit):
@@ -529,7 +488,6 @@ class ConfBlog:
 
 
 class ConfBracket:
-
     """The class ConfBracket is responsible for bracketing the root within
     the interval (a,b) where f(a)*f(b) < 0.0"""
 
@@ -672,7 +630,6 @@ class ConfBracket:
 
 
 class ConfRootNone:
-
     """The base class for the root of the confidence interval"""
 
     def __init__(self, root=None):
@@ -691,7 +648,6 @@ class ConfRootNone:
 
 
 class ConfRootBracket(ConfRootNone):
-
     """The class contains the bracket where the confidence root has
     been bracketed, ie where f(a)*f(b) < 0"""
 
@@ -758,7 +714,6 @@ class ConfRootBracket(ConfRootNone):
 
 
 class ConfRootZero(ConfRootNone):
-
     """The class with the root/zero of the confidence interval"""
 
     def __str__(self):
