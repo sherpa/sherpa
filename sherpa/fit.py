38077a0fc9cbc3d5415521174d0ed0b89b89209c
Douglas Burke <dburke.gw@gmail.com>
2025-03-09 15:02:16 -0400

    statargs/kwargs arguments for statistic calls are now unused
    Adding types for the various optimization-related code has shown
    partial support for sending in extra arguments to routines that
    calculate the statistic value for a set of parameters. Examples
    include that the estmethods code allows both statargs and statkwargs
    to be set but does nothing with this information, and some code
    allows for extra positional arguments but not named ones.
    
    This makes following the code, and adding types, harder than it
    could be, in part because the code is written in a functional
    style (in that functions are passed around) and there are a lot
    of places where "adapters" are needed to support different
    calling styles (or extracting out a subset of the return data).
    
    So these changes
    
    - remove passing in extra arguments to these callback-like
      functions
    
    - changes the default values for the statargs and statkwargs to
      None
    
      (recent work actually did this for most/maybe all of the
       statkwargs parameters, since they used to be set to a default
       of '{}' and pylint complained about using a mutable argument,
       e.g. PR #1778)
    
    - creates a WARNING-level log message if the routines are ever
      called with statargs or statkwargs set, to let users know this
      information is not being used
    
    Now, if users do need to send in extra arguments then it is
    suggested that they create a new function that adds in the extra
    data (e.g. follows the callback-like approach used by these
    routines).
    
    Note that there were are tests that exercised the "send in extra
    information with statargs or statkwargs" functionality.

--- sherpa/fit.py
+++ sherpa/fit.py
@@ -793,8 +793,8 @@ class IterFit:
                  pars: ArrayType,
                  parmins: ArrayType,
                  parmaxes: ArrayType,
-                 statargs: Sequence[Any] = (),
-                 statkwargs: Mapping[str, Any] | None = None,
+                 statargs: Any = None,
+                 statkwargs: Any = None,
                  cache: bool = True
                  ) -> OptReturn:
         """Exclude points that are significately far away from the best fit.
@@ -806,6 +806,9 @@ class IterFit:
         has converged or a maximum number of iterations has been reached.
         The error removal can be asymmetric, since there are separate
         options for the lower and upper limits.
+
+        .. versionchanged:: 4.17.1
+           The statargs and statkwargs arguments are now ignored.
 
         Raises
         ------
@@ -830,8 +833,9 @@ class IterFit:
         ========  ==========  ===========
 
         """
-        if statkwargs is None:
-            statkwargs = {}
+
+        if statargs is not None or statkwargs is not None:
+            warning("statargs/kwargs set but values unused")
 
         # Sigma-rejection can only be used with chi-squared;
         # raise exception if it is attempted with least-squares,
@@ -908,9 +912,9 @@ class IterFit:
                     self.stat.calc_staterror)
                 self.model.startup(cache)
                 final_fit_results = self.method.fit(statfunc,
-                                                    self.model.thawedpars,
-                                                    parmins, parmaxes,
-                                                    statargs, statkwargs)
+                                                    pars=self.model.thawedpars,
+                                                    parmins=parmins,
+                                                    parmaxes=parmaxes)
                 model_iterator = iter(self.model())
                 rejected = False
 
@@ -1006,19 +1010,25 @@ class IterFit:
             pars: ArrayType,
             parmins: ArrayType,
             parmaxes: ArrayType,
-            statargs: Sequence[Any] = (),
-            statkwargs: Mapping[str, Any] | None = None,
+            statargs: Any = None,
+            statkwargs: Any = None
             ) -> OptReturn:
+        """
 
-        if statkwargs is None:
-            statkwargs = {}
+        .. versionchanged:: 4.17.1
+           The statargs and statkwargs arguments are now ignored.
+
+        """
+
+        if statargs is not None or statkwargs is not None:
+            warning("statargs/kwargs set but values unused")
 
         if self.current_func is None:
-            return self.method.fit(statfunc, pars, parmins, parmaxes,
-                                   statargs, statkwargs)
+            return self.method.fit(statfunc, pars=pars,
+                                   parmins=parmins, parmaxes=parmaxes)
 
-        return self.current_func(statfunc, pars, parmins, parmaxes,
-                                 statargs, statkwargs)
+        return self.current_func(statfunc, pars=pars, parmins=parmins,
+                                 parmaxes=parmaxes)
 
 
 # What is the best way to annotate the return value here?
