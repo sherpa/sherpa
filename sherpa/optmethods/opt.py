245896d17a0edb7bf115df42558709b91cbf518c
Douglas Burke <dburke.gw@gmail.com>
2025-03-20 08:44:48 -0400

    opt: internal rework of multi-core code
    The multi-core code here does not quite match the versions in
    sherpa.utils.parallel, which is why they do not use those functions.
    However, the code can be written to better use the parallel module: in
    this case the code was confusing to follow since the run_tasks routine
    was written assuming it would be sent tasks, each of which contained
    multiple tasks, but here wach task is a single job, so the "result
    expansion" was hard to follow and even harder to type. So tweak the
    code to make the results of the calc call easier to type, and so
    easier to process.
    
    There is the question of whther the work queue should be sharded by
    the numcores argument (which is currently ignored). It seems like it
    would make sense but it is a larger change than I would want to make
    here. See issue #2267.

--- sherpa/optmethods/opt.py
+++ sherpa/optmethods/opt.py
@@ -71,7 +71,8 @@ class MyNcores:
              xmax: np.ndarray,
              tol: float,
              maxnfev: int | None
-             ) -> list:
+             ) -> list[MyOptOutput]:
+        """Apply each function to the arguments, running in parallel."""
 
         for func in funcs:
             if not callable(func):
@@ -80,6 +81,8 @@ class MyNcores:
         # See sherpa.utils.parallel for the logic used here.
         # At this point we can assume that context is not None,
         # since multi is True.
+        #
+        # Should this shard by numcores?
         #
         assert context is not None
         manager = context.Manager()
@@ -96,7 +99,7 @@ class MyNcores:
     def my_worker(self,
                   opt: WorkerFunc,
                   idval: int,
-                  out_q: SupportsQueue[tuple[int, list]],
+                  out_q: SupportsQueue[tuple[int, list[MyOptOutput]]],
                   err_q: SupportsQueue[Exception],
                   fcn: OptimizerFunc,
                   x: np.ndarray,
