f3fabf1572518fc040ee2c0f11a9304091b7468d
Doug Burke <dburke@cfa.harvard.edu>
2025-06-25 14:10:06 -0400

    Remove unused code in sherpa.utils.integration C++
    Remove unused code and extra spaces.

--- sherpa/utils/src/integration.cc
+++ sherpa/utils/src/integration.cc
@@ -1,5 +1,6 @@
-// 
-//  Copyright (C) 2007, 2016, 2020  Smithsonian Astrophysical Observatory
+//
+//  Copyright (C) 2007, 2016, 2020, 2025
+//  Smithsonian Astrophysical Observatory
 //
 //
 //  This program is free software; you can redistribute it and/or modify
@@ -27,15 +28,11 @@
 #include "adapt_integrate.h"
 #include "qng.h"
 
-extern "C" {
-  void initintegration();
-}
-
 namespace sherpa { namespace integration {
 
   static int gsl_int_flag = 1;
   static int sao_int_flag = 1;
-  
+
   int integrate_1d( integrand_1d fct, void* params,
 		    double xlo, double xhi,
 		    unsigned int maxeval, double epsabs, double epsrel,
@@ -53,20 +50,20 @@ namespace sherpa { namespace integration {
     size_t neval = size_t( maxeval );
 
     gsl_set_error_handler_off ();
- 
+
     retval = gsl_integration_qng( &func, xlo, xhi, epsabs, epsrel, &result,
 				  &abserr, &neval );
-    
+
     if (retval != EXIT_SUCCESS) {
       if( gsl_int_flag ) {
 	std::cerr << "WARNING: Gauss-Kronrod integration failed "
 		  << "with tolerance " << epsabs << ", trying lower tolerance..."
 		  << std::endl;
-	
+
 	double tol = std::numeric_limits< float >::epsilon();
 	retval = gsl_integration_qng( &func, xlo, xhi, tol, epsrel, &result,
 				      &abserr, &neval );
-	
+
 	if (retval != EXIT_SUCCESS) {
 	  std::cerr << "integration failed with tolerance " << tol
 		    << ", resorting to trapezoid method"
@@ -79,9 +76,9 @@ namespace sherpa { namespace integration {
 		    << std::endl;
 	}
 	gsl_int_flag = 0;
-      }      
+      }
     }
-    
+
     return EXIT_SUCCESS;
 
   }
@@ -119,37 +116,37 @@ namespace sherpa { namespace integration {
     size_t neval = size_t( maxeval );
 
     gsl_set_error_handler_off ();
- 
+
     retval = sao_integration_qng( fct, xlo, xhi, params, epsabs, epsrel,
 				  &result, &abserr, &neval );
-    
+
     if (retval == -1) {
       return EXIT_FAILURE;
     }
-    
+
     if (retval != EXIT_SUCCESS) {
       if (sao_int_flag) {
 	err << "Gauss-Kronrod integration failed "
 	    << "with tolerance " << epsabs << ", trying lower tolerance...";
-	
+
 	double tol = std::numeric_limits< float >::epsilon();
 	retval = sao_integration_qng( fct, xlo, xhi, params, tol, epsrel,
 				      &result, &abserr, &neval );
-	
+
 	if (retval != EXIT_SUCCESS) {
 	  err << std::endl << "integration failed with tolerance " << tol
 	      << ", resorting to trapezoid method";
 
-	  
+
 	  double loval[1], hival[1];
 	  loval[0] = xlo;
 	  hival[0] = xhi;
 	  if (-1 == fct(loval, 1, params) )
 	    return EXIT_FAILURE;
-	  
+
 	  if (-1 == fct(hival, 1, params) )
 	    return EXIT_FAILURE;
-	  
+
 	  result = 0.5 * ( xhi - xlo ) * ( loval[0] + hival[0] );
 	}
 	else {
@@ -159,9 +156,9 @@ namespace sherpa { namespace integration {
       sao_int_flag=0;
     }
     return EXIT_SUCCESS;
-    
+
   }
-    
+
 }  }  /* namespace integration, namespace sherpa */
 
 static struct PyModuleDef integration = {
