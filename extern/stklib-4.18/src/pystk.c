0023871b1595f2aef49242851e5fd7374ca96b97
Doug Burke <dburke@cfa.harvard.edu>
2025-06-25 16:23:22 -0400

    extern: mark stk as supporting free threading
    This assumes that the stack code is thread safe.
    
    The code also drops the Python 2 support, as this is not needed.

--- extern/stklib-4.18/src/pystk.c
+++ extern/stklib-4.18/src/pystk.c
@@ -26,33 +26,6 @@
 #include <unistd.h>
 #include <stdio.h>
 
-/** 
- * For supporting both python 2 and 3, we include macros that abstract out
- * differences in module initialization and other issues described in:
- * 
- * http://python3porting.com/cextensions.html
- */
-#if PY_MAJOR_VERSION >= 3
-#define MOD_ERROR_VAL NULL
-#define MOD_SUCCESS_VAL(val) val
-#define MOD_INIT(name) PyMODINIT_FUNC PyInit_##name(void)
-#define MOD_DEF(ob, name, doc, methods) \
-        static struct PyModuleDef moduledef = { \
-          PyModuleDef_HEAD_INIT, name, doc, -1, methods, NULL, NULL, NULL, NULL }; \
-        ob = PyModule_Create(&moduledef);
-#define MOD_NEWOBJ(newval, val) newval = PyCapsule_New((void *)val, NULL, NULL);
-#define PyString_FromString PyUnicode_FromString
-#else
-#define MOD_ERROR_VAL
-#define MOD_SUCCESS_VAL(val)
-#define MOD_INIT(name) PyMODINIT_FUNC init##name(void)
-#define MOD_DEF(ob, name, doc, methods) \
-        ob = Py_InitModule3(name, methods, doc);
-#define MOD_NEWOBJ(newval, val) newval = PyCObject_FromVoidPtr((void*)val, NULL);
-#endif
-
-MOD_INIT(stk);
-
 static PyMethodDef stkMethods[] =
 {
   /* Each entry in the groupMethods array is a PyMethodDef structure containing
@@ -61,7 +34,7 @@ static PyMethodDef stkMethods[] =
    * 3) flags indicating whether or not keywords are accepted for this function,
    * and 4) The docstring for the function.
    */
-  { "build", (PyCFunction)_stk_build, METH_VARARGS, 
+  { "build", (PyCFunction)_stk_build, METH_VARARGS,
     "Example: \n"
     ">>> foo = build(\"a,b,c,d\")\n"
     ">>> foo\n"
@@ -73,16 +46,29 @@ static PyMethodDef stkMethods[] =
 /*
  * Initialize the module
  * */
-MOD_INIT(stk)
+#if PY_MAJOR_VERSION >= 3
+#define MOD_DEF(ob, name, doc, methods) \
+        static struct PyModuleDef moduledef = { \
+          PyModuleDef_HEAD_INIT, name, doc, -1, methods }; \
+        ob = PyModule_Create(&moduledef);
+#endif
+
+PyMODINIT_FUNC PyInit_stk(void)
 {
-  PyObject *mod = NULL;
-  
-  MOD_DEF(mod, "stk", "stk", stkMethods)
+
+  static struct PyModuleDef moduledef = {
+    PyModuleDef_HEAD_INIT, "stk", "stk", -1, stkMethods };
+
+  PyObject *mod = PyModule_Create(&moduledef);
   if (mod == NULL) {
-    return MOD_ERROR_VAL;
+    return NULL;
   }
-  
-  return MOD_SUCCESS_VAL(mod);
+
+#ifdef Py_GIL_DISABLED
+  PyUnstable_Module_SetGIL(mod, Py_MOD_GIL_NOT_USED);
+#endif
+
+  return mod;
 }
 
 /* * * * * * * * * * * * * * * * * * * * * * * *
@@ -95,12 +81,12 @@ MOD_INIT(stk)
  *   Uses stk library to open and process the provided stack list
  *   generating a List of the stack contents.
  *
- * Returns 
+ * Returns
  *   Python List containing all stack entries.
  */
 static PyObject* _stk_build(PyObject *self, PyObject *args)
 {
-  char *buff; 
+  char *buff;
   int  status = 0;
   (void)self; /* avoid compiler warning */
 
@@ -118,7 +104,7 @@ static PyObject* _stk_build(PyObject *self, PyObject *args)
       PyErr_Restore(type, value, traceback);
       status = 1;
     }
-  } 
+  }
 #else
   if (!PyArg_ParseTuple(args, "s", &buff))
     status = 1;
@@ -131,14 +117,14 @@ static PyObject* _stk_build(PyObject *self, PyObject *args)
     return NULL;
   }
 
-  if ( ( NULL == buff ) || ( 0 == strlen( buff ) ) ) 
+  if ( ( NULL == buff ) || ( 0 == strlen( buff ) ) )
   {
     PyErr_SetString(PyExc_Exception, "Empty stack string.");
     return NULL;
   }
 
   Stack *stk;
-  
+
   /* We trash stk_lib's stdout/stderr and issue our own exception */
 
   int pipe_fild[2];
@@ -146,10 +132,10 @@ static PyObject* _stk_build(PyObject *self, PyObject *args)
   int orig_err = dup( fileno(stderr ));
   fflush(stderr);
   dup2( pipe_fild[1], fileno(stderr));
-  
+
   /* Open the stack */
   stk = stk_build( buff );
-  
+
   /* Return stderr */
   fflush(stderr);
   dup2( orig_err, fileno(stderr));
@@ -158,7 +144,7 @@ static PyObject* _stk_build(PyObject *self, PyObject *args)
   close(orig_err);
 
   /* Check stack opened OK */
-  if ( NULL == stk ) 
+  if ( NULL == stk )
   {
     long maxl = strlen( buff ) + 100;
     char *ibuff = ( char*) calloc( maxl, sizeof(char));
@@ -175,8 +161,8 @@ static PyObject* _stk_build(PyObject *self, PyObject *args)
     PyErr_SetString(PyExc_ValueError, "Stack has 0 elements");
     return NULL;
   }
-  
-  if ( (1 == num_elem ) && (0 == strlen( stk_read_num( stk, 1 )) )) 
+
+  if ( (1 == num_elem ) && (0 == strlen( stk_read_num( stk, 1 )) ))
   {
     PyErr_SetString(PyExc_ValueError, "Stack has only 1 element and it is blank");
     return NULL;
@@ -184,7 +170,7 @@ static PyObject* _stk_build(PyObject *self, PyObject *args)
 
   /* Create output List */
   PyObject *pylist;
-  if ( NULL == (pylist = PyList_New(  (Py_ssize_t) 0 ))) 
+  if ( NULL == (pylist = PyList_New(  (Py_ssize_t) 0 )))
   {
     PyErr_SetString(PyExc_Exception, "Failed to create new list");
     return NULL;
@@ -192,19 +178,19 @@ static PyObject* _stk_build(PyObject *self, PyObject *args)
 
   /* Transfer Stack content to List */
   int ii;
-  for ( ii = 1; ii <= num_elem; ii++ ) 
+  for ( ii = 1; ii <= num_elem; ii++ )
   {
     /* get stack record */
     char *ibuff = stk_read_num( stk, ii );
-    if ( NULL == ibuff ) 
-    {	
+    if ( NULL == ibuff )
+    {
       PyErr_SetString(PyExc_IndexError, "Invalid stack_read_num");
       return NULL;
     }
-    
+
     /* Convert to python string object */
-    PyObject *pstr = PyString_FromString( ibuff );
-    if ( NULL == pstr ) 
+    PyObject *pstr = PyUnicode_FromString( ibuff );
+    if ( NULL == pstr )
     {
       PyErr_SetString(PyExc_ValueError, "Cannot convert to python string");
       return NULL;
@@ -216,12 +202,12 @@ static PyObject* _stk_build(PyObject *self, PyObject *args)
       PyErr_SetString( PyExc_Exception, "Failed to append string to list");
       return NULL;
     }
-    
+
     stk_read_free( ibuff );
 
   } /* End for ii */
 
   stk_close( stk );
-  
+
   return( pylist );
 }
