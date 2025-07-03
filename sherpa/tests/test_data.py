233340ce72db15eb8cf8e185b7c69e6eec10cac2
Doug Burke <dburke@cfa.harvard.edu>
2025-07-03 14:45:42 -0400

    data: to_guess/fit/plot now requires both axes to be set
    Previously to_guess would return None for each unset axis, but now it
    raises a DataErr, since the assumption is that there is valid data to
    guess. There is an attempt to remain compatible with the existing
    error messages, but there may be subtle changes (normally a change
    in the DataErr message, but this is not guaranteed [*]).
    
    [*] e.g. DataPHA("x", [1, 2, 3], None).to_guess() would have errored
        out as it tried to divide an array by None. It now raises a
        DataErr with a message that the dependent axis has not been set.
    
    Similar changes are made to to_fit and to_plot. There is also the
    removal of a to_fit method in DataPHA since it didn't add anything
    compared to its superclass.
    
    The Fit.fit and .est_errors methods have been reworked to handle the
    new behaviour but retain the old behaviour - e.g. to throw a DataErr
    or FitErr. This has meant we can remove the internal _check_length
    routine, as it's no-longer needed.

--- sherpa/tests/test_data.py
+++ sherpa/tests/test_data.py
@@ -632,10 +632,9 @@ def test_data_1d_to_fit(data):
 def test_data_1d_to_fit_no_data(data_class, args):
     """Regression test"""
     data = data_class("empty", *args)
-    dep, staterr, syserr = data.to_fit()
-    assert dep is None
-    assert staterr is None
-    assert syserr is None
+    with pytest.raises(DataErr,
+                       match="^The size of 'empty' has not been set$"):
+        _ = data.to_fit()
 
 
 @pytest.mark.parametrize("x,y", [([1, 2], None), (None, [1, 2])])
@@ -646,11 +645,14 @@ def test_to_fit_data1d_one_axis_empty(x, y):
     # worth the complexity?
     #
     data = Data1D("test", x, y)
-    yf, _, _ = data.to_fit()
     if x is None:
+        yf, _, _ = data.to_fit()
         assert yf == pytest.approx(y)
-    else:
-        assert yf is None
+        return
+
+    with pytest.raises(DataErr,
+                       match="^The size of 'test' has not been set$"):
+        _ = data.to_fit()
 
 
 @pytest.mark.parametrize("data", (Data1D, ), indirect=True)
@@ -686,8 +688,10 @@ def test_data_1d_to_plot_no_data(data_class, args):
         pytest.xfail("Data1DInt data access when no independent axis")
 
     data = data_class("empty", *args)
-    res = data.to_plot()
-    assert res[0] is None
+    with pytest.raises(DataErr,
+                       match="^The independent axis of 'empty' has "
+                       "not been set$"):
+        _ = data.to_plot()
 
 
 @pytest.mark.parametrize("x,y", [([1, 2], None), (None, [1, 2])])
@@ -698,13 +702,10 @@ def test_to_plot_data1d_one_axis_empty(x, y):
     # worth the complexity?
     #
     data = Data1D("test", x, y)
-    xp, yp, _, _, _, _ = data.to_plot()
-    if x is None:
-        assert yp == pytest.approx(y)
-        assert xp is None
-    else:
-        assert yp is None
-        assert xp == pytest.approx(x)
+    label = "" if y is None else "in"
+    msg = f"The {label}dependent axis of 'test' has not been set"
+    with pytest.raises(DataErr, match=f"^{msg}$"):
+        _ = data.to_plot()
 
 
 @pytest.mark.parametrize("data_class,args", EMPTY_DATA_OBJECTS_1D)
@@ -714,8 +715,10 @@ def test_data_1d_to_component_plot_no_data(data_class, args):
         pytest.xfail("Data1DInt data access when no independent axis")
 
     data = data_class("empty", *args)
-    res = data.to_component_plot()
-    assert res[0] is None
+    with pytest.raises(DataErr,
+                       match="^The independent axis of 'empty' has "
+                       "not been set$"):
+        _ = data.to_component_plot()
 
 
 @pytest.mark.parametrize("x,y", [([1, 2], None), (None, [1, 2])])
@@ -726,13 +729,10 @@ def test_to_component_plot_data1d_one_axis_empty(x, y):
     # worth the complexity?
     #
     data = Data1D("test", x, y)
-    xp, yp, _, _, _, _ = data.to_component_plot()
-    if x is None:
-        assert yp == pytest.approx(y)
-        assert xp is None
-    else:
-        assert yp is None
-        assert xp == pytest.approx(x)
+    label = "" if y is None else "in"
+    msg = f"The {label}dependent axis of 'test' has not been set"
+    with pytest.raises(DataErr, match=f"^{msg}$"):
+        _ = data.to_component_plot()
 
 
 @pytest.mark.parametrize("data", (Data, Data1D, Data1DInt), indirect=True)
@@ -2786,14 +2786,9 @@ def test_to_guess_when_empty(data_class, args):
         pytest.xfail("test known to fail with Data1DInt")
 
     data = data_class("empty", *args)
-    resp = data.to_guess()
-
-    # Ensure there are n None values, where n is the number of
-    # independent + dependent axes - ie len(args)
-    #
-    assert len(resp) == len(args)
-    for r in resp:
-        assert r is None
+    with pytest.raises(DataErr,
+                       match="^The size of 'empty' has not been set$"):
+        _ = data.to_guess()
 
 
 @pytest.mark.parametrize("x,y", [([1, 2], None), (None, [1, 2])])
@@ -2804,13 +2799,10 @@ def test_to_guess_data1d_one_axis_empty(x, y):
     # worth the complexity?
     #
     data = Data1D("test", x, y)
-    yg, xg = data.to_guess()
-    if x is None:
-        assert yg == pytest.approx(y)
-        assert xg is None
-    else:
-        assert yg is None
-        assert xg == pytest.approx(x)
+    label = "" if y is None else "in"
+    msg = f"The {label}dependent axis of 'test' has not been set"
+    with pytest.raises(DataErr, match=f"^{msg}$"):
+        _ = data.to_guess()
 
 
 @pytest.mark.parametrize("data_copy", ALL_DATA_CLASSES, indirect=True)
