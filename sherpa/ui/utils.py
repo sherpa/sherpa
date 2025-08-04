b5875313435d3390d2ea65736b137acf34695c33
Douglas Burke <dburke.gw@gmail.com>
2025-08-04 19:18:58 -0400

    sample: doc and internal code cleanup
    Try to normalize the "scales" documentation.
    
    Apply some minimal code clean-ups
    
    - e.g. no need to convert something to a ndarray when we know
      it's already a ndarray
    
    - no need to transpose a 1D ndarray
    
    - be explicit about paramter names for get_scales/myscales

--- sherpa/ui/utils.py
+++ sherpa/ui/utils.py
@@ -10204,7 +10204,7 @@ class Session(NoNewAttributesAfterInit):
         parameters. The median value of the statistic calculated by
         `uniform_sample` is returned:
 
-        >>> ans = uniform_sample(num=10000)
+        >>> ans = uniform_sample(num=1000)
         >>> ans.shape
         (1000, 4)
         >>> np.median(ans[:,0])
@@ -10271,7 +10271,7 @@ class Session(NoNewAttributesAfterInit):
         parameters. The median value of the statistic calculated by
         `t_sample` is returned:
 
-        >>> ans = t_sample(num=10000)
+        >>> ans = t_sample(num=1000)
         >>> ans.shape
         (1000, 4)
         >>> np.median(ans[:,0])
