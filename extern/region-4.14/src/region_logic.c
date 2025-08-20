fa888bd7cffc745eba07e5bd8161d83f013d7e70
Warren McLaughlin <wmclaugh@cfa.harvard.edu>
2025-08-20 15:24:57 -0400

    copyright updates to be consistent with CIAO codebase

--- extern/region-4.14/src/region_logic.c
+++ extern/region-4.14/src/region_logic.c
@@ -1,7 +1,6 @@
 /*                                                                
-**  Copyright (C) 2007,2013  Smithsonian Astrophysical Observatory 
+**  Copyright (C) 2015,2018  Smithsonian Astrophysical Observatory 
 */                                                                
-
 /*                                                                          */
 /*  This program is free software; you can redistribute it and/or modify    */
 /*  it under the terms of the GNU General Public License as published by    */
