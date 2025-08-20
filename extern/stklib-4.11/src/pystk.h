af9436ed81e22199f0d521badc2ee79a2b3560ed
Warren McLaughlin <wmclaugh@cfa.harvard.edu>
2025-08-20 15:49:00 -0400

    L-569 - python module support for numpy2

--- extern/stklib-4.11/src/pystk.h
+++ extern/stklib-4.11/src/pystk.h
@@ -1,7 +1,6 @@
 /*                                                                
-**  Copyright (C) 2011  Smithsonian Astrophysical Observatory 
+**  Copyright (C) 2011,2016  Smithsonian Astrophysical Observatory 
 */                                                                
-
 /*                                                                          */
 /*  This program is free software; you can redistribute it and/or modify    */
 /*  it under the terms of the GNU General Public License as published by    */
