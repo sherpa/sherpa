UI: calculate covariance within get_draws; fix #556
We assume that covar() is not an expensive call to make, so we
now *always* calculate the covariance matrix within get_draws, rather
than rely on the user having called covar first.

This should make things a bit easier for the user, as well as fixing
problems like #556 - where it was possible to get the wrong answer by
not calling covar first.

There is the question of whether the user should see this covariance
output, but I decided against it.

We also do not store the results of the "covar" call, since this could
confuse existing code (although if it did this would suggest that the
code was actually subtly wrong).

A different option would be to identify when the covar results are
not valid and only call covariance in this case, but that turns
out to be more awkward than it seems, and it is just easier to
always re-calculate the matrix.

sherpa/astro/ui/utils.py
@@ -15380,10 +15380,11 @@ class Session(sherpa.ui.utils.Session):
                 id: IdType | None = None,
                 lo=None, hi=None,
                 bkg_id: IdType | None = None,
-                error=False, params=None,
+                error: bool = False,
+                params: np.ndarray | None = None,
                 otherids: IdTypes = (),
-                niter=1000,
-                covar_matrix=None):
+                niter: int = 1000,
+                covar_matrix: np.ndarray | None = None):
         """Calculate the equivalent width of an emission or absorption line.
 
         The `equivalent width <https://en.wikipedia.org/wiki/Equivalent_width>`_
@@ -15530,6 +15531,8 @@ class Session(sherpa.ui.utils.Session):
                 if covar_matrix is None:
                     try:
                         # check just in case usr has run covar()
+                        # TODO: should this validate the covariance
+                        # results?
                         covar_results = self.get_covar_results()
                         covar_matrix = covar_results.extra_output
                     except SessionErr:

sherpa/ui/tests/test_ui_unit.py
@@ -2521,7 +2521,7 @@ def test_get_draws_multiple_datasets(order, clean_ui):
     setup_multiple_datasets()
 
     ui.fit(*order)
-    ui.covar(*order)
+    # ui.covar(*order)  as of 4.18.0 this is not needed
 
     # Ensure repeatability (unlike the _linked version the covariance
     # matrix is the same, so this is repeatable).
@@ -2549,7 +2549,7 @@ def test_get_draws_multiple_datasets_linked(order, parnames, clean_ui):
     setup_multiple_datasets_linked()
 
     ui.fit(*order)
-    ui.covar(*order)
+    # ui.covar(*order)  as of 4.18.0 this is not needed
 
     # Ensure repeatability. However, unlike the fit and covar results,
     # we do not get the same results.
@@ -2576,15 +2576,15 @@ def test_get_draws_mismatched_covar_linked(clean_ui, caplog):
 
     # Not only are the datasets swapped in the covar call, only one
     # component is asked for, and not all of them. This does not
-    # actually matter since the covariance matrix is calculated
-    # for all the free parameters.
+    # actually matter since get_draws recalculates the covariance
+    # matrix.
     #
     ui.fit(1, 2)
     ui.covar(2, 1, scale.c0)
 
     ui.set_rng(np.random.RandomState(3287))
     n1 = len(caplog.records)
-    stats, accept, params = ui.get_draws(1, [2], niter=5)
+    stats, accept, params = ui.get_draws(1, [2], niter=100)
     n2 = len(caplog.records)
 
     # Check the expected extra messages.
@@ -2594,7 +2594,13 @@ def test_get_draws_mismatched_covar_linked(clean_ui, caplog):
     assert caplog.records[n1 + 1].getMessage().startswith("pol1.c1: ")
     assert caplog.records[n1 + 2].getMessage().startswith("pol2.c0: ")
 
-    # The covariance matrix is invalid but has the correct shape,
-    # so the code runs. However, the parameters do not move.
+    # Unlike test_get_draws_multiple_datasets_linked we know the
+    # ordering and can be much more precise in the tests.
     #
-    assert accept == pytest.approx([False] * 6)
+    means = params.mean(axis=1)
+    assert means[0] == pytest.approx(1.73371932)
+    assert means[1] == pytest.approx(-10.46427734)
+    assert means[2] == pytest.approx(22.00661571)
+
+    # There's at least one accept.
+    assert any(accept)

sherpa/ui/utils.py
@@ -70,6 +70,7 @@ from sherpa.utils import NoNewAttributesAfterInit, is_subclass, \
 from sherpa.utils.err import ArgumentErr, ArgumentTypeErr, \
     DataErr, IdentifierErr, IOErr, ModelErr, ParameterErr, PlotErr, \
     SessionErr
+from sherpa.utils.logging import SherpaVerbosity
 from sherpa.utils.numeric_types import SherpaFloat
 from sherpa.utils.random import RandomType
 from sherpa.utils.types import ArrayType, IdType, IdTypes, PrefsType
@@ -12016,21 +12017,28 @@ class Session(NoNewAttributesAfterInit):
         """
         return self._pyblocxs.list_samplers()
 
-    # DOC-TODO: add pointers on what to do with the return values
     def get_draws(self,
                   id: IdType | None = None,
                   otherids: IdTypes = (),
-                  niter=1000, covar_matrix=None):
+                  niter: int = 1000,
+                  covar_matrix: np.ndarray | None = None
+                  ) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
         """Run the pyBLoCXS MCMC algorithm.
 
         The function runs a Markov Chain Monte Carlo (MCMC) algorithm
         designed to carry out Bayesian Low-Count X-ray Spectral
         (BLoCXS) analysis. It explores the model parameter space at
-        the suspected statistic minimum (i.e.  after using `fit`). The return
-        values include the statistic value, parameter values, and an
-        acceptance flag indicating whether the row represents a jump from the
-        current location or not. For more information see the
-        `sherpa.sim` module and the reference given below.
+        the suspected statistic minimum (i.e.  after using `fit`). The
+        return values include the statistic value, parameter values,
+        and an acceptance flag indicating whether the row represents a
+        jump from the current location or not. For more information
+        see the `sherpa.sim` module and the reference given below.
+
+        .. versionchanged:: 4.18.0
+           If ``covar_matrix`` is left unset then the covariance
+           matrix is now always re-calculated. This means that
+           ``covar`` is no-longer needed to be called before this
+           routine.
 
         Parameters
         ----------
@@ -12043,7 +12051,8 @@ class Session(NoNewAttributesAfterInit):
            The number of draws to use. The default is ``1000``.
         covar_matrix : 2D array, optional
            The covariance matrix to use. If ``None`` then the
-           result from `get_covar_results().extra_output` is used.
+           covariance matrix from the current parameter values
+           is calculated.
 
         Returns
         -------
@@ -12102,10 +12111,9 @@ class Session(NoNewAttributesAfterInit):
         results.
 
         >>> fit()
-        >>> covar()
         >>> stats, accept, params = get_draws(1, niter=1e4)
         >>> plot_trace(stats, name='stat')
-        >>> names = [p.fullname for p in get_source().pars if not p.frozen]
+        >>> names = [p.fullname for p in get_source().get_thawed_pars()]
         >>> plot_cdf(params[0,:], name=names[0], xlabel=names[0])
         >>> plot_pdf(params[1,:], name=names[1], xlabel=names[1])
         >>> accept[:-1].sum() * 1.0 / len(accept - 1)
@@ -12114,23 +12122,21 @@ class Session(NoNewAttributesAfterInit):
         The following runs the chain on multiple data sets, with
         identifiers 'core', 'jet1', and 'jet2':
 
-        >>> stats, accept, params = get_draws('core', ['jet1', 'jet2'], niter=1e4)
+        >>> res = get_draws('core', ['jet1', 'jet2'], niter=1e4)
+        >>> stats, accept, params = res
 
         """
 
         ids, fit = self._get_fit(id, otherids)
 
-        # Allow the user to jump from a user defined point in parameter space?
-        # Meaning let the user set up parameter space without fitting first.
-
-        # fit_results = self.get_fit_results()
-        # if fit_results is None:
-        #    raise TypeError("Fit has not been run")
-
         if covar_matrix is None:
-            covar_results = self.get_covar_results()
-            if covar_results is None:
-                raise TypeError("Covariance has not been calculated")
+            # Always recalculate the covariance matrix, even if covar
+            # has just been called, but hide the screen output. This
+            # data is not stored so as to not confuse users about what
+            # data get_covar_results() is returning.
+            #
+            with SherpaVerbosity("ERROR"):
+                covar_results = self._est_errors(ids, 'covariance')
 
             covar_matrix = covar_results.extra_output
 