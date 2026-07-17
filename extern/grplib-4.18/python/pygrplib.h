960edb849cdc7151a7e8f5ff3970682376ff0225
Doug Burke <dburke.gw@gmail.com>
2026-07-17 10:00:37 -0400

    BUILD: internal clean up
    The pygrplib.h change can be explained by the fact that the
    Python module has changed what it includes over time (particularly
    with the limited API turned on). Why it happened now and not
    earlier is unclear.
    
    The explicit version checks oni the extern meson.build files are
    from noting warning messages from a build.

--- extern/grplib-4.18/python/pygrplib.h
+++ extern/grplib-4.18/python/pygrplib.h
@@ -1,5 +1,5 @@
 /*                                                                
-**  Copyright (C) 2007,2015  Smithsonian Astrophysical Observatory 
+**  Copyright (C) 2007,2015,2026  Smithsonian Astrophysical Observatory
 */                                                                
 /*                                                                          */
 /*  This program is free software; you can redistribute it and/or modify    */
@@ -39,6 +39,7 @@ H***************************************************************** */
 #define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
 
 #include "Python.h"
+#include <stdio.h>
 #include <numpy/arrayobject.h>  /* Used by NumPy */
 
 /* * * * * * * * * * * * * * * * * * * * * * * *
